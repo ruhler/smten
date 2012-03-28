2665bdf686aed4baffdedc24772a8b32ea7005c7
Richard Uhler <ruhler@csail.mit.edu>
2012-03-28 14:06:04 -0400

    Removed TypeCheck and TypeInfer.
    Which are no longer needed because we have haskell to do that work for us.

--- src/Seri.hs
+++ src/Seri.hs
@@ -2,23 +2,17 @@
 {-# LANGUAGE QuasiQuotes #-}
 
 module Seri (
-    module Seri.Parser,
-    module Seri.Quoter,
-    module Seri.TypeCheck,
-    module Seri.TypeInfer,
-    module Seri.IR,
     module Seri.Elaborate,
+    module Seri.IR,
+    module Seri.Quoter,
+    module Seri.Typed,
     tests
     ) where
 
 import Seri.Elaborate
 import Seri.IR
-import Seri.Parser
 import Seri.Quoter
-import Seri.TypeCheck
-import Seri.TypeInfer
 import Seri.Typed
-import Seri.TypedQuoter
 
 import Test.HUnit
 
@@ -33,46 +27,12 @@ foo =
      lam = LamE (ArrowT IntegerT IntegerT) "x" body
  in AppE IntegerT lam (IntegerE 5)
 
--- Run type inference and type checking on the expression,
--- Then elaborate it.
-run :: (Monad m) => Exp -> m Exp
-run exp = do
-    let inferred = typeinfer exp
-    typecheck inferred
-    return $ elaborate inferred
-
 runt :: TypedExp a -> Exp
 runt = elaborate . typed
-
-is :: Exp -> Either String Exp
-is = Right
 
 tests = "Seri" ~: [
     "Elaborate" ~: [
         "simple" ~: IntegerE 42 ~=? elaborate foo
-        ],
-    "Quoter" ~: [
-        "simple" ~: IntegerE 42 ~=? elaborate [s|(\x -> x*x+3*x+2) 5|],
-        "slice" ~:  IntegerE 47 ~=? elaborate [s|(\x -> x*x+@(toInteger $ length [1, 2, 3, 4])*x+2) 5|],
-        "nested" ~: IntegerE 142 ~=? 
-            let muln :: Integer -> Exp -> Exp
-                muln 1 x = x
-                muln n x = MulE x $ muln (n-1) x
-
-                t = muln 3 [s|x|]
-            in elaborate [s|(\x -> @(t)+3*x+2) 5|]
-        ],
-    "TypeCheck" ~: [
-        "simple good" ~: Just IntegerT ~=? typecheck foo,
-        "bad_app" ~: Nothing ~=?
-            let body = VarE IntegerT "x"
-                lam = LamE (ArrowT IntegerT IntegerT) "x" body
-                exp = AppE (ArrowT IntegerT IntegerT) lam (IntegerE 5)
-            in typecheck exp,
-        "uninferred" ~: Nothing ~=? typecheck [s|(\x -> x+2)|]
-        ],
-    "TypeInfer" ~: [
-        "simple" ~: foo ~=? typeinfer [s|(\x -> x*x+3*x+2) 5|]
         ],
     "Typed" ~: [
         "lt" ~: BoolE True ~=?
@@ -90,7 +50,7 @@ tests = "Seri" ~: [
                 exp = typed texp
             in elaborate exp
         ],
-    "TypedQuoter" ~: [
+    "General" ~: [
         "simple" ~: IntegerE 42 ~=? runt [st|(\x -> x*x+3*x+2) 5|],
         "true" ~: BoolE True ~=? runt [st| true |],
         "if" ~: IntegerE 23 ~=? runt [st| if 6 < 4 then 42 else 23 |],
@@ -98,20 +58,6 @@ tests = "Seri" ~: [
         "fix" ~: IntegerE 120 ~=?
             let factorial = [st| !f \x -> if (x < 1) then 1 else x * f (x-1) |]
             in runt [st| @(factorial) 5 |]
-        ],
-    "General" ~: [
-        "simple" ~: is (IntegerE 42) ~=? run [s|(\x -> x*x+3*x+2) 5|],
-        "frontspace" ~: is (IntegerE 13) ~=? run [s| 8+5|],
-        "backspace" ~: is (IntegerE 13) ~=? run [s|8+5 |],
-        "space" ~: is (IntegerE 13) ~=? run [s| 8 + 5 |],
-        "subtract" ~: is (IntegerE 3) ~=? run [s| 8 - 5 |],
-        "true" ~: is (BoolE True) ~=? run [s| true |],
-        "false" ~: is (BoolE False) ~=? run [s| false |],
-        "lt" ~: is (BoolE False) ~=? run [s| 6 < 4 |],
-        "if" ~: is (IntegerE 23) ~=? run [s| if 6 < 4 then 42 else 23 |],
-        "fix" ~: is (IntegerE 120) ~=?
-            let factorial = [s| !f \x -> if (x < 1) then 1 else x * f (x-1) |]
-            in run [s| @(factorial) 5 |]
         ]
     ]
 
--- src/Seri/Quoter.hs
+++ src/Seri/Quoter.hs
@@ -1,49 +1,208 @@
 
 {-# LANGUAGE TemplateHaskell #-}
 
-module Seri.Quoter (s)
-    where
+module Seri.Quoter (
+    st
+    ) where
+
+import Data.Char(ord)
+import Text.Parsec hiding (token)
+import Language.Haskell.Meta.Parse(parseExp)
 
 import Language.Haskell.TH.Quote
-import Language.Haskell.TH.Syntax (Lift(..))
-import qualified Language.Haskell.TH as TH
+import Language.Haskell.TH
 
-import Seri.Parser
-import Seri.IR
+import qualified Seri.IR as SIR
+import qualified Seri.Typed as S
 
-s :: QuasiQuoter 
-s = QuasiQuoter qexp qpat qtype qdec
+-- Parse a seri expression.
+-- Fails if there is a parse error.
+seriparse :: String -> Q Exp
+seriparse str
+  = case (runParser top () "???" str) of
+        Left err -> fail $ show (errorPos err) ++ ": " ++ show err
+        Right x -> return x
 
-qexp :: String -> TH.Q TH.Exp
-qexp str = seriparse str >>= lift
+type Parser = Parsec String ()
 
-qpat :: String -> TH.Q TH.Pat
+apply :: Name -> [Exp] -> Exp
+apply n exps = foldl AppE (VarE n) exps
+
+atom :: Parser Exp
+atom = do
+    e <- (eth <|> elam <|> efix <|> eparen <|> einteger <|>
+          eif <|> try etrue <|> try efalse <|> try evar)
+    many space
+    return e
+
+appls :: Parser Exp
+appls = atom `chainl1` eapp
+
+mults :: Parser Exp
+mults = appls `chainl1` emul
+
+adds :: Parser Exp
+adds = mults `chainl1` (eadd <|> esub)
+
+lts :: Parser Exp
+lts = adds `chainl1` elt
+
+expression :: Parser Exp
+expression = lts
+
+-- top level parser, skips initial whitespace, force match at eof.
+top :: Parser Exp
+top = do
+    many space
+    x <- expression
+    eof
+    return x
+
+-- A string token followed by optional space.
+token :: String -> Parser String
+token x = do
+    string x
+    many space
+    return x
+
+eparen :: Parser Exp
+eparen = do
+    token "("
+    x <- expression
+    token ")"
+    return x
+
+etrue :: Parser Exp
+etrue = do
+    token "true"
+    return $ apply 'S.boolE [ConE 'True]
+
+efalse :: Parser Exp
+efalse = do
+    token "false"
+    return $ apply 'S.boolE [ConE 'False]
+
+eif :: Parser Exp
+eif = do
+    token "if"
+    p <- expression
+    token "then"
+    tb <- expression
+    token "else"
+    tf <- expression
+    return $ apply 'S.ifE [p, tb, tf]
+
+einteger :: Parser Exp
+einteger = do
+    x <- integer
+    many space
+    return $ apply 'S.integerE [LitE (IntegerL x)]
+
+integer :: Parser Integer
+integer = do 
+    digits <- many1 digit
+    return $ digitstoint 0 digits
+
+digitstoint :: Integer -> [Char] -> Integer
+digitstoint acc [] = acc
+digitstoint acc (x:xs) = digitstoint (acc*10 + (fromIntegral $ (ord x - ord '0'))) xs
+    
+eadd :: Parser (Exp -> Exp -> Exp)
+eadd = do
+    token "+"
+    return $ (\a b -> apply 'S.addE [a, b])
+
+esub :: Parser (Exp -> Exp -> Exp)
+esub = do
+    token "-"
+    return $ (\a b -> apply 'S.subE [a, b])
+
+elt :: Parser (Exp -> Exp -> Exp)
+elt = do
+    token "<"
+    return $ (\a b -> apply 'S.ltE [a, b])
+
+emul :: Parser (Exp -> Exp -> Exp)
+emul = do
+    token "*"
+    return $ (\a b -> apply 'S.mulE [a, b])
+
+eapp :: Parser (Exp -> Exp -> Exp)
+eapp = do
+    return $ (\a b -> apply 'S.appE [a, b])
+
+elam :: Parser Exp
+elam = do
+    char '\\'
+    nm <- name
+    many space
+    token "->"
+    body <- expression
+    return $ apply 'S.lamE [LitE (StringL nm), LamE [VarP $ mkName nm] body]
+
+efix :: Parser Exp
+efix = do
+    char '!'
+    nm <- name
+    many space
+    body <- expression
+    return $ apply 'S.fixE [LitE (StringL nm), LamE [VarP $ mkName nm] body]
+
+
+reserved = ["if", "then", "else", "true", "false"]
+
+evar :: Parser Exp
+evar = do
+    nm <- name
+    if (nm `elem` reserved)
+     then fail ""
+     else do
+        many space
+        return $ VarE (mkName nm)
+
+name :: Parser SIR.Name
+name = many1 alphaNum
+
+-- Parse a template haskell slice
+-- @(...)
+-- Where ... is haskell code with properly nested parentheses.
+eth :: Parser Exp
+eth = do
+    string "@("
+    str <- strtoclose 1
+    case parseExp str of
+        Right x -> return $ x
+        Left err -> fail err
+
+strtoclose :: Integer -> Parser String
+strtoclose n = do
+    c <- anyChar
+    case (c, n) of
+        (')', 1) -> return ""
+        (')', _) -> do
+            s <- strtoclose (n-1)
+            return (c:s)
+        ('(', _) -> do
+            s <- strtoclose (n+1)
+            return (c:s)
+        (_, _) -> do
+            s <- strtoclose n
+            return (c:s)
+    
+
+st :: QuasiQuoter 
+st = QuasiQuoter qexp qpat qtype qdec
+
+qexp :: String -> Q Exp
+qexp = seriparse
+
+qpat :: String -> Q Pat
 qpat = error $ "Seri pattern quasi-quote not supported"
 
-qtype :: String -> TH.Q TH.Type
+qtype :: String -> Q Type
 qtype = error $ "Seri type quasi-quote not supported"
 
-qdec :: String -> TH.Q [TH.Dec]
+qdec :: String -> Q [Dec]
 qdec = error $ "Seri dec quasi-quote not supported"
 
-
-instance Lift Type where
-    lift BoolT = [e| BoolT |]
-    lift IntegerT = [e| IntegerT |]
-    lift (ArrowT a b) = [e| ArrowT a b |]
-    lift UnknownT = [e| UnknownT |]
-
-instance Lift Exp where
-    lift (BoolE x) = [e| BoolE x |]
-    lift (IntegerE x) = [e| IntegerE x |]
-    lift (AddE a b) = [e| AddE a b |]
-    lift (SubE a b) = [e| SubE a b |]
-    lift (MulE a b) = [e| MulE a b |]
-    lift (LtE a b) = [e| LtE a b |]
-    lift (IfE t p a b) = [e| IfE t p a b |]
-    lift (AppE t a b) = [e| AppE t a b |]
-    lift (FixE t n e) = [e| FixE t n e |]
-    lift (LamE t n e) = [e| LamE t n e |]
-    lift (VarE t n) = [e| VarE t n |]
-    lift (ThE x) = TH.appE [e| seriate |] (return x)
 
