d1bea31ede6a5b6392da92c7e99d4ca25ee59734
Richard Uhler <ruhler@csail.mit.edu>
2012-03-28 13:59:03 -0400

    Removing generalized version of Seri.
    Because I like the specific one better, and I want to work with that to start.

--- src/Seri/Name.hs
+++
@@ -1,6 +0,0 @@
-
-module Seri.Name (Name)
-    where
-
-type Name = String
-
--- src/Seri/Ppr.hs
+++
@@ -1,9 +0,0 @@
-
-module Seri.Ppr (
-    module Language.Haskell.TH.Ppr,
-    module Language.Haskell.TH.PprLib
-    ) where
-
-import Language.Haskell.TH.Ppr
-import Language.Haskell.TH.PprLib
-
