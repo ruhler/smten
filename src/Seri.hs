7f4fff5f98e88ed7b6bf57f230813bf26908127f
Richard Uhler <ruhler@csail.mit.edu>
2012-03-29 10:17:21 -0400

    Tested recursive factorial implementation.

--- src/Seri.hs
+++ src/Seri.hs
@@ -46,6 +46,11 @@ rund decls = elaborate decls . typed
     fact6 = 6 * fact5
 |]
 
+[s|
+    rfact :: Integer -> Integer
+    rfact = \x -> if (x < 1) then 1 else x * rfact (x-1)
+|]
+
 tests = "Seri" ~: [
     "foo" ~: IntegerE 42 ~=? run [s|(\x -> x*x+3*x+2) 5|],
     "true" ~: PrimE BoolT TrueP ~=? run [s| True |],
@@ -56,6 +61,7 @@ tests = "Seri" ~: [
         in run [s| @(factorial) 5 |],
     "foo decl" ~: IntegerE 42 ~=? run _seri__foo,
     "fact5 decl" ~: IntegerE 120 ~=? rund _serictx_fact5 _seri__fact5,
-    "subctx" ~: IntegerE 720 ~=? rund _serictx_fact6 _seri__fact6
+    "subctx" ~: IntegerE 720 ~=? rund _serictx_fact6 _seri__fact6,
+    "rfact5" ~: IntegerE 120 ~=? rund _serictx_rfact [s| rfact 5 |]
     ]
 
