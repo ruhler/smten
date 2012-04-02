e9bbc27fc92f1ff2f36e6625b222a6d0601ed31b
Richard Uhler <ruhler@csail.mit.edu>
2012-04-01 20:26:03 -0400

    Implemented Quoter which uses haskell parser.

--- src/Seri.hs
+++ src/Seri.hs
@@ -13,6 +13,7 @@ module Seri (
 import Seri.Elaborate
 import Seri.IR
 import Seri.Quoter
+import Seri.HSQuoter
 import Seri.Primitives
 import Seri.Typed
 
@@ -57,13 +58,20 @@ run decls = elaborate decls . typed
 
 tests = "Seri" ~: [
     "foo" ~: IntegerE 42 ~=? run [] [s|(\x -> x*x+3*x+2) 5|],
+    "foohs" ~: IntegerE 42 ~=? run [] [hs|(\x -> x*x+3*x+2) 5|],
     "unit" ~: PrimE UnitT UnitP ~=? run _seriD_unit [s| unit |],
     "true" ~: PrimE (ConT "Bool") TrueP ~=? run _seriD_True [s| True |],
+    "truehs" ~: PrimE (ConT "Bool") TrueP ~=? run _seriD_True [hs| True |],
     "if" ~: IntegerE 23 ~=? run [] [s| if 6 < 4 then 42 else 23 |],
+    "ifhs" ~: IntegerE 23 ~=? run [] [hs| if 6 < 4 then 42 else 23 |],
     "slice" ~: IntegerE 7 ~=? run [] [s| 3 + @(integerE . toInteger $ length [4,1,5,56]) |],
+    "slicehs" ~: IntegerE 7 ~=? run [] [hs| 3 + _s (integerE . toInteger $ length [4,1,5,56]) |],
     "fix" ~: IntegerE 120 ~=?
         let factorial = [s| fix (\f -> \x -> if (x < 1) then 1 else x * f (x-1)) |]
         in run _seriD_fix [s| @(factorial) 5 |],
+    "fixhs" ~: IntegerE 120 ~=?
+        let factorial = [hs| fix (\f -> \x -> if (x < 1) then 1 else x * f (x-1)) |]
+        in run _seriD_fix [hs| _s (factorial) 5 |],
     "foo decl" ~: IntegerE 42 ~=? run [] _seriC_foo,
     "fact5 decl" ~: IntegerE 120 ~=? run _seriD_fact5 _seriC_fact5,
     "subctx" ~: IntegerE 720 ~=? run _seriD_fact6 _seriC_fact6,
---
+++ src/Seri/HSQuoter.hs
@@ -0,0 +1,112 @@
+
+{-# LANGUAGE TemplateHaskell #-}
+
+module Seri.HSQuoter ( hs
+    ) where
+
+import Control.Monad.State
+
+import Language.Haskell.TH
+import Language.Haskell.TH.Quote
+import Language.Haskell.Meta.Parse(parseExp)
+
+import qualified Seri.IR as SIR
+import qualified Seri.Typed as S
+
+import Seri.Declarations
+
+data UserState = UserState {
+    boundnames :: [Name],
+    freenames :: [Name]
+}
+
+initialUserState = UserState [] []
+
+freename :: Name -> State UserState ()
+freename nm = modify (\us -> us { freenames = (nm:(freenames us)) })
+
+bindname :: Name -> State UserState ()
+bindname nm = modify (\us -> us { boundnames = (nm:(boundnames us)) })
+
+unbindname :: Name -> State UserState ()
+unbindname nm = do
+    UserState (n:names) fn <- get
+    if (n /= nm) 
+        then fail $ "unbindname '" ++ show nm ++ "' doesn't match expected '" ++ show n ++ "'"
+        else put $ UserState names fn
+
+
+apply :: Name -> [Exp] -> Exp
+apply n exps = foldl AppE (VarE n) exps
+
+infixp :: Name -> Exp -> Exp -> Exp
+infixp nm a b = apply 'S.infixE [VarE nm, a, b]
+
+-- mkexp :: Exp (a) -> Exp (S.TypedExp a)
+--   Convert a haskell expression to its corresponding typed seri
+--   representation.
+--
+--   This supports only those haskell expressions which can be represented in
+--   the seri IR.
+mkexp :: Exp -> State UserState Exp 
+mkexp (VarE nm) = do
+    bound <- gets boundnames
+    if (nm `elem` bound)
+        then return $ VarE nm
+        else do
+            freename nm
+            return $ apply 'S.varE_typed [VarE (name_P (nameBase nm)), LitE (StringL (nameBase nm))]
+
+mkexp (ConE nm) = return $ apply 'S.varE_typed [VarE (name_P (nameBase nm)), LitE (StringL (nameBase nm))]
+
+mkexp l@(LitE (IntegerL i)) = return $ apply 'S.integerE [l]
+
+-- Special case for slices.
+mkexp (AppE (VarE s) b) | s == mkName "_s"  = return b
+
+mkexp (AppE a b) = do
+    a' <- mkexp a
+    b' <- mkexp b
+    return $ apply 'S.appE [a', b']
+
+mkexp (InfixE (Just a) (VarE op) (Just b)) = do
+    a' <- mkexp a
+    b' <- mkexp b
+    return $ case (nameBase op) of
+        "+" -> infixp 'S.addP a' b'
+        "-" -> infixp 'S.subP a' b'
+        "*" -> infixp 'S.mulP a' b'
+        "<" -> infixp 'S.ltP a' b'
+        x -> error $ "TODO: infix " ++ show x
+
+mkexp (LamE [VarP nm] a) = do
+    bindname nm
+    a' <- mkexp a
+    unbindname nm
+    return $ apply 'S.lamE [LitE (StringL (nameBase nm)), LamE [VarP nm] a']
+
+mkexp (CondE p a b) = do
+    p' <- mkexp p
+    a' <- mkexp a
+    b' <- mkexp b
+    return $ apply 'S.ifE [p', a', b']
+
+
+hs :: QuasiQuoter 
+hs = QuasiQuoter qexp qpat qtype qdec
+
+qexp :: String -> Q Exp
+qexp s = case (parseExp s) of
+            Right e -> return (fst $ runState (mkexp e) initialUserState)
+            Left err -> fail err
+
+qpat :: String -> Q Pat
+qpat = error $ "Seri pattern quasi-quote not supported"
+
+qtype :: String -> Q Type
+qtype = error $ "Seri type quasi-quote not supported"
+
+qdec :: String -> Q [Dec]
+qdec = error $ "Seri dec quasi-quote not supported"
+
+
