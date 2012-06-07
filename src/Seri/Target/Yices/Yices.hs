9c5f130a22ff1a37e3df9bb5ccac9c334e40e0b6
Richard Uhler <ruhler@csail.mit.edu>
2012-06-07 10:56:52 -0400

    Implemented (untested) compilation of DataD to yices.

--- src/Seri/Target/Yices/Yices.hs
+++ src/Seri/Target/Yices/Yices.hs
@@ -1,6 +1,7 @@
 
-module Seri.Target.Yices.Yices (yicesY) where
+module Seri.Target.Yices.Yices (yicesY, compile_decs) where
 
+import Data.Maybe(fromJust)
 import qualified Math.SMT.Yices.Syntax as Y
 
 import Seri.Lambda
@@ -22,3 +23,21 @@ coreY = Compiler yExp yType
 yicesY :: Compiler
 yicesY = compilers [preludeY, coreY]
             
+-- compile_dec
+--   Assumes the declaration is monomorphic.
+compile_dec :: Compiler -> Dec -> [Y.CmdY]
+compile_dec c (ValD (Sig n t) e) =
+    let Just yt = compile_type c c t
+        Just (cmds, ye) = compile_exp c c e
+    in cmds ++ [Y.DEFINE (n, yt) (Just ye)]
+compile_dec c (DataD n [] cs) =
+    let con :: Con -> (String, [(String, Y.TypY)])
+        con (Con n ts) = (n, zip [n ++ show i | i <- [0..]]
+                                 (map (fromJust . compile_type c c) ts))
+    in [Y.DEFTYP n (Just (Y.DATATYPE (map con cs)))]
+compile_dec c d
+    = error $ "compile_dec: cannot compile to yices: " ++ render (ppr d)
+
+compile_decs :: Compiler -> [Dec] -> [Y.CmdY]
+compile_decs c ds = concat $ map (compile_dec c) ds
+
