16c8ed15fa9aa5c52d2d6c4d9cc78b45305b153f
Richard Uhler <ruhler@csail.mit.edu>
2012-06-08 14:17:29 -0400

    Changed to using Num.
    Requires some support for monomorphizing in yices which hasn't been
    implemented yet.

--- src/Seri/Target/Yices/Builtins/Integer.hs
+++ src/Seri/Target/Yices/Builtins/Integer.hs
@@ -31,8 +31,8 @@ yIncludes = [
     ]
 
 yExp :: Compiler -> Exp -> Maybe ([Y.CmdY], Y.ExpY)
-yExp c (PrimE (Sig "+" _)) = return ([], Y.VarE "__prim_add")
-yExp c (PrimE (Sig "-" _)) = return ([], Y.VarE "__prim_sub")
+yExp c (PrimE (Sig "__prim_add_Integer" _)) = return ([], Y.VarE "__prim_add")
+yExp c (PrimE (Sig "__prim_sub_Integer" _)) = return ([], Y.VarE "__prim_sub")
 yExp c (PrimE (Sig "<" _)) = return ([], Y.VarE "__prim_lt")
 yExp c (PrimE (Sig ">" _)) = return ([], Y.VarE "__prim_gt")
 yExp c (PrimE (Sig "==" _)) = return ([], Y.VarE "__prim_eq")
