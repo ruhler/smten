a404e85b2097bd3b46ea86f42b3b655b24a5926e
Richard Uhler <ruhler@csail.mit.edu>
2012-06-08 09:47:01 -0400

    Bug fixes.
    Empty case assertions were generating (assert (or (and ))).
    Types were improperly compiled to yices.
    Symbol identifiers are renamed to something yices supports.

--- src/Seri/Target/Yices/Yices.hs
+++ src/Seri/Target/Yices/Yices.hs
@@ -28,6 +28,18 @@ yExp c (CaseE e ms) =
       depat (IntegerP i) e = ([Y.LitI i Y.:= e], [])
       depat (WildP _) _ = ([], [])
 
+      -- take the AND of a list of predicates in a reasonable way.
+      yand :: [Y.ExpY] -> Y.ExpY
+      yand [] = Y.VarE "true"
+      yand [x] = x
+      yand xs = Y.AND xs
+
+      -- take the OR of a list of predicats in a reasonable way.
+      yor :: [Y.ExpY] -> Y.ExpY
+      yor [] = Y.VarE "false"
+      yor [x] = x
+      yor xs = Y.OR xs
+
       -- dematch e ms
       --    e - the expression being cased on
       --    ms - the remaining matches in the case statement.
@@ -40,17 +52,17 @@ yExp c (CaseE e ms) =
       dematch e [Match p b] = 
         let Just (cmds, b') = compile_exp c c b
             (pred, bindings) = depat p e
-        in (cmds, [Y.AND pred], Y.LET bindings b')
+        in (cmds, [yand pred], Y.LET bindings b')
       dematch e ((Match p b):ms) =
         let (cms, pms, bms) = dematch e ms
             Just (cmds, b') = compile_exp c c b
             (preds, bindings) = depat p e
-            pred = Y.AND preds
+            pred = yand preds
         in (cmds ++ cms, pred:pms, Y.IF pred (Y.LET bindings b') bms)
   in do
       (es, e') <- compile_exp c c e
       let (cmds, ps, b) = dematch e' ms
-      return (cmds ++ [Y.ASSERT (Y.OR ps)], b)
+      return (cmds ++ [Y.ASSERT (yor ps)], b)
 yExp c (AppE a b) = do
     (as, a') <- compile_exp c c a
     (bs, b') <- compile_exp c c b
@@ -58,13 +70,42 @@ yExp c (AppE a b) = do
 yExp c (LamE (Sig n t) e) = do
     (es, e') <- compile_exp c c e
     return (es, Y.LAMBDA [(n, fromJust $ compile_type c c t)] e')
-yExp _ (ConE (Sig n _)) = return ([], Y.VarE n)
-yExp _ (VarE (Sig n _) _) = return ([], Y.VarE n)
+yExp _ (ConE (Sig n _)) = return ([], Y.VarE (yicesname n))
+yExp _ (VarE (Sig n _) _) = return ([], Y.VarE (yicesname n))
 yExp _ _ = Nothing
+
+-- Given a seri identifer, turn it into a valid yices identifier.
+-- TODO: hopefully our choice of names won't clash with the users choices...
+--
+-- I don't have documentation for what yices allows in names, but it appears
+-- symbols aren't allowed. So this just replaces each symbol with an ascii
+-- approximation.
+yicesname :: String -> String
+yicesname [] = []
+yicesname ('!':cs) = "__bang" ++ yicesname cs
+yicesname ('#':cs) = "__hash" ++ yicesname cs
+yicesname ('$':cs) = "__dollar" ++ yicesname cs
+yicesname ('%':cs) = "__percent" ++ yicesname cs
+yicesname ('&':cs) = "__amp" ++ yicesname cs
+yicesname ('*':cs) = "__star" ++ yicesname cs
+yicesname ('+':cs) = "__plus" ++ yicesname cs
+yicesname ('.':cs) = "__dot" ++ yicesname cs
+yicesname ('/':cs) = "__slash" ++ yicesname cs
+yicesname ('<':cs) = "__lt" ++ yicesname cs
+yicesname ('=':cs) = "__eq" ++ yicesname cs
+yicesname ('>':cs) = "__gt" ++ yicesname cs
+yicesname ('?':cs) = "__ques" ++ yicesname cs
+yicesname ('@':cs) = "__at" ++ yicesname cs
+yicesname ('\\':cs) = "__bslash" ++ yicesname cs
+yicesname ('^':cs) = "__hat" ++ yicesname cs
+yicesname ('|':cs) = "__bar" ++ yicesname cs
+yicesname ('-':cs) = "__dash" ++ yicesname cs
+yicesname ('~':cs) = "__tilde" ++ yicesname cs
+yicesname (c:cs) = c : yicesname cs
 
 yType :: Compiler -> Type -> Maybe Y.TypY
 yType _ (ConT n) = Just $ Y.VarT n
-yType c (AppT a b) = do
+yType c (AppT (AppT (ConT "->") a) b) = do
     a' <- compile_type c c a
     b' <- compile_type c c b
     return $ Y.ARR [a', b']
@@ -82,7 +123,7 @@ compile_dec :: Compiler -> Dec -> [Y.CmdY]
 compile_dec c (ValD (Sig n t) e) =
     let yt = fromMaybe (error $ "compile type " ++ render (ppr t)) (compile_type c c t)
         (cmds, ye) = fromMaybe (error $ "compile exp " ++ render (ppr e)) (compile_exp c c e)
-    in cmds ++ [Y.DEFINE (n, yt) (Just ye)]
+    in cmds ++ [Y.DEFINE (yicesname n, yt) (Just ye)]
 compile_dec c (DataD n [] cs) =
     let con :: Con -> (String, [(String, Y.TypY)])
         con (Con n ts) = (n, zip [n ++ show i | i <- [0..]]
