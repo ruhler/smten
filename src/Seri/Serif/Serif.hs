f5d95ae7d665defcea410088b964c470ae7da74c
Richard Uhler <ruhler@csail.mit.edu>
2012-06-11 17:19:55 -0400

    Implemented translation of simple expression.
    I'll need to try compiling it with haskell next to make sure it works (I'm
    sure it doesn't).

--- src/Seri/Serif/Serif.hs
+++ src/Seri/Serif/Serif.hs
@@ -1,4 +1,6 @@
 
+{-# LANGUAGE TemplateHaskell #-}
+
 module Seri.Serif.Serif (serif)
   where
 
@@ -6,10 +8,47 @@ module Seri.Serif.Serif (serif)
 import qualified Language.Haskell.TH as H
 import Seri.Lambda
 import Seri.Utils.Ppr
+import Seri.FrontEnd.Typed
 
 serif :: [Dec] -> [H.Dec]
 serif = concat . map mkdec  
 
+-- mkexp bound e
+--  bound - a list of bound names.
+mkexp :: [Name] -> Exp -> H.Exp
+mkexp bound (VarE (Sig n _) _) | n `elem` bound
+ = H.VarE (mknm n)
+mkexp bound (VarE (Sig n _) _) 
+ = apply 'dvarE [H.VarE (vnm n), H.VarE (iidnm n), string n]
+mkexp _ (IntegerE i)
+ = apply 'integerE [H.LitE (H.IntegerL i)]
+mkexp bound (AppE a b)
+ = apply 'appE [mkexp bound a, mkexp bound b]
+mkexp bound (LamE (Sig n _) e)
+ = apply 'lamE [string n, H.LamE [H.VarP (mknm n)] (mkexp (n:bound) e)]
+mkexp _ e = error $ "TODO: mkexp: " ++ render (ppr e)
+
+mknm :: Name -> H.Name
+mknm = H.mkName
+
+mkty :: Type -> H.Type
+mkty (ConT nm) = H.ConT (mknm nm)
+mkty t = error $ "TODO: mkty: " ++ render (ppr t)
+
 mkdec :: Dec -> [H.Dec]
+mkdec (ValD (Sig n t) e)
+ = [H.SigD (mknm n) (mkty t), H.ValD (H.VarP (mknm n)) (H.NormalB (mkexp [] e)) []]
 mkdec d = error $ "TODO: mkdec: " ++ render (ppr d)
+
+apply :: H.Name -> [H.Exp] -> H.Exp
+apply n es = foldl H.AppE (H.VarE n) es
+
+string :: Name -> H.Exp
+string n = H.LitE (H.StringL n)
+
+vnm :: Name -> H.Name
+vnm n = H.mkName ("serifP_" ++ n)
+
+iidnm :: Name -> H.Name
+iidnm n = H.mkName ("serifI_" ++ n)
 
