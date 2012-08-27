d98cf4416dff4b0d20529c294c7f2d1f40b5e46c
Richard Uhler <ruhler@csail.mit.edu>
2012-08-26 23:23:16 -0400

    Join short lines in yices concrete.

--- src/Yices/Syntax.hs
+++ src/Yices/Syntax.hs
@@ -237,22 +237,29 @@ instance Concrete Command where
     concrete v Push = ["(push)"]
     concrete v Pop = ["(pop)"]
 
+concrete' :: (Concrete a) => YicesVersion -> a -> [String]
+concrete' v x =
+  let lines = concrete v x
+  in if (sum (map length lines) < 72)
+        then [concat lines]
+        else lines
+
 instance Concrete [Command] where
-    concrete v cmds = concat $ map (concrete v) cmds
+    concrete v cmds = concat $ map (concrete' v) cmds
 
 instance Concrete Typedef where
     concrete v (ScalarTD ss) = ["(scalar " ++ concat (map ((:) ' ') ss) ++ ")"]
-    concrete v (NormalTD t) = concrete v t
+    concrete v (NormalTD t) = concrete' v t
 
 instance Concrete Type where
     concrete v (VarT s) = [s]
     concrete v (TupleT ts)
       = ["(tuple"]
-        ++ indent (concat $ map (concrete v) ts)
+        ++ indent (concat $ map (concrete' v) ts)
         ++ [")"]
     concrete v (ArrowT ts)
       = ["(->"]
-        ++ indent (concat $ map (concrete v) ts)
+        ++ indent (concat $ map (concrete' v) ts)
         ++ [")"]
     concrete v (BitVectorT i) = ["(bitvector " ++ show i ++ ")"]
     concrete v IntegerT = ["int"]
@@ -260,45 +267,45 @@ instance Concrete Type where
     concrete v RealT = ["real"]
 
 instance Concrete Expression where
-    concrete v (ImmediateE iv) = concrete v iv
+    concrete v (ImmediateE iv) = concrete' v iv
     concrete v (ForallE decls e)
         = ["(forall"]
-          ++ indent (["("] ++ indent (concat $ map (concrete v) decls) ++ [")"])
-          ++ indent (concrete v e)
+          ++ indent (["("] ++ indent (concat $ map (concrete' v) decls) ++ [")"])
+          ++ indent (concrete' v e)
           ++ [")"]
     concrete v (ExistsE decls e)
         = ["(exists"]
-          ++ indent (["("] ++ indent (concat $ map (concrete v) decls) ++ [")"])
-          ++ indent (concrete v e)
+          ++ indent (["("] ++ indent (concat $ map (concrete' v) decls) ++ [")"])
+          ++ indent (concrete' v e)
           ++ [")"]
     concrete v (LetE bindings e)
         = ["(let"]
-          ++ indent (["("] ++ indent (concat $ map (concrete v) bindings) ++ [")"])
-          ++ indent (concrete v e)
+          ++ indent (["("] ++ indent (concat $ map (concrete' v) bindings) ++ [")"])
+          ++ indent (concrete' v e)
           ++ [")"]
     concrete v (UpdateE f es e)
         = ["(update"]
-          ++ indent (concrete v f)
-          ++ indent (["("] ++ indent (concat $ map (concrete v) es) ++ [")"])
-          ++ indent (concrete v e)
+          ++ indent (concrete' v f)
+          ++ indent (["("] ++ indent (concat $ map (concrete' v) es) ++ [")"])
+          ++ indent (concrete' v e)
           ++ [")"]
     concrete v (TupleUpdateE t i x)
         = [(if v == Yices1 then "(update" else "(tuple-update")]
-          ++ indent (concrete v t)
+          ++ indent (concrete' v t)
           ++ indent [show i]
-          ++ indent (concrete v x)
+          ++ indent (concrete' v x)
           ++ [")"]
     concrete v (FunctionE f args)
         = ["("]
-          ++ indent (concrete v f)
-          ++ indent (concat $ map (concrete v) args)
+          ++ indent (concrete' v f)
+          ++ indent (concat $ map (concrete' v) args)
           ++ [")"]
 
 instance Concrete VarDecl where
-    concrete v (n, t) = [n ++ " ::"] ++ indent (concrete v t)
+    concrete v (n, t) = [n ++ " ::"] ++ indent (concrete' v t)
 
 instance Concrete Binding where
-    concrete v (n, e) = ["(" ++ n] ++ indent (concrete v e) ++ [")"]
+    concrete v (n, e) = ["(" ++ n] ++ indent (concrete' v e) ++ [")"]
 
 instance Concrete ImmediateValue where
     concrete v TrueV = ["true"]
@@ -312,5 +319,5 @@ instance Concrete ImmediateValue where
 
 -- | Render abstract yices syntax to a concrete syntax string.
 pretty :: Concrete a => YicesVersion -> a -> String
-pretty v x = unlines (concrete v x)
+pretty v x = unlines (concrete' v x)
 
