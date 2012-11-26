f1a07b180433e4dfd2585ffca34d514adcaa6add
Richard Uhler <ruhler@csail.mit.edu>
2012-11-26 07:46:38 -0500

    Cache top level declarations already translated to ExpH.
    This makes use of unsafePerformIO, I believe in a safe way.
    Improves performance a noticable amount. Like 13 down to 9, or 38 down to 26.

--- seri/Seri/ExpH/EnvH.hs
+++ seri/Seri/ExpH/EnvH.hs
@@ -9,10 +9,13 @@ import Debug.Trace
 import Control.Monad
 import Data.Either
 import Data.List(nub)
+import qualified Data.Map as Map
+
+import Data.IORef
+import System.IO.Unsafe
 
 import Seri.Name
 import Seri.Sig
-import qualified Seri.HashTable as HT
 import Seri.Failable
 import Seri.Type
 import Seri.Dec
@@ -24,72 +27,26 @@ import Seri.ExpH.Utils
 
 data EnvH = EnvH {
     eh_env :: Env,
-    eh_l1 :: HT.HashTable Sig ExpH,
-    eh_l2 :: HT.HashTable Name (Type -> Maybe ExpH)
+    eh_cache :: IORef (Map.Map Sig (Maybe ExpH))
 }
 
 mkEnvH :: Env -> EnvH
-mkEnvH e = 
-  let isconcrete :: Type -> Bool
-      isconcrete t = null (nvarTs t) && null (varTs t)
+mkEnvH e = EnvH e (unsafePerformIO (newIORef Map.empty))
 
-      getcvarsD :: Dec -> [Sig]
-      getcvarsD (ValD (TopSig n _ t) e) =
-        let evs = getcvarsE [] e
-        in if isconcrete t
-             then (Sig n t) : evs
-             else evs
-      getcvarsD (InstD _ _ ms) = concat [getcvarsE [] e | Method _ e <- ms]
-      getcvarsD _ = []
-
-      getcvarsE :: [Name] -> Exp -> [Sig]
-      getcvarsE _ (LitE {}) = []
-      getcvarsE _ (ConE {}) = []
-      getcvarsE bnd (VarE s@(Sig n t)) | n `notElem` bnd && isconcrete t = [s]
-      getcvarsE _ (VarE {}) = []
-      getcvarsE bnd (AppE a b) = getcvarsE bnd a ++ getcvarsE bnd b
-      getcvarsE bnd (LamE (Sig n _) x) = getcvarsE (n:bnd) x
-      getcvarsE bnd (CaseE x _ y n) = concat [
-            getcvarsE bnd x, getcvarsE bnd y, getcvarsE bnd n]
-
-      getcelem :: Sig -> [(Sig, ExpH)]
-      getcelem s@(Sig n ct) = attemptM $ do
-        (pt, ve) <- lookupVar e s
-        return $ (s, toExpH (assignments pt ct) [] ve)
-
-      getelem :: Dec -> [Either (Sig, ExpH) (Name, Type -> Maybe ExpH)]
-      getelem (ValD (TopSig n _ pt) e) | isconcrete pt = [Left (Sig n pt, toExpH [] [] e)]
-      getelem (ValD (TopSig n _ pt) e) =
-        let f ct = return $ toExpH (assignments pt ct) [] e
-        in [Right (n, f)]
-      getelem (ClassD _ _ tss) = [Right (n, mkf n) | TopSig n _ _ <- tss]
-      getelem _ = []
-
-      elems = concatMap getelem (getDecls e)
-
-      mkf :: Name -> Type -> Maybe ExpH
-      mkf n ct = do
-        (pt, ve) <- attemptM $ lookupVar e (Sig n ct)
-        return $ toExpH (assignments pt ct) [] ve
-
-      --allcvars = nub $ concatMap getcvarsD (getDecls e)
-      allcvars = []
-      specialize = []
---        Sig (name "Prelude.++") (arrowsT [stringT, stringT, stringT]),
---        Sig (name "Prelude.concat") (arrowsT [listT stringT, stringT]),
---        Sig (name "Prelude.curry")
---            (arrowsT [arrowsT [tupleT [charT, charT], charT],
---                      charT, charT, charT])
---         ]
-
-      l1 = HT.table (lefts elems ++ concatMap getcelem (specialize ++ allcvars))
-      l2 = HT.table (rights elems)
-  in EnvH e l1 l2
-
+lookupIO :: EnvH -> Sig -> IO (Maybe ExpH)
+lookupIO (EnvH e r) s@(Sig n ct) = do
+    m <- readIORef r
+    case Map.lookup s m of
+        Just v -> return v
+        Nothing -> do
+           let x = attemptM $ do
+                     (pt, ve) <- lookupVar e s
+                     return $ toExpH (assignments pt ct) [] ve
+           writeIORef r (Map.insert s x m)
+           return x
+                       
 lookupVarH :: EnvH -> Sig -> Maybe ExpH
-lookupVarH e s@(Sig n ct) = mplus (HT.lookup s (eh_l1 e)) $ do
-    f <- HT.lookup n (eh_l2 e)
-    f ct
+lookupVarH e s = unsafePerformIO (lookupIO e s)
     
 onEnv :: (Env -> a) -> EnvH -> a
 onEnv f = f . eh_env
