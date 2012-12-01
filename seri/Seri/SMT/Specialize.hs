f63a5316bdc0c295412b7de7683fd50c2e0c1c2b
Richard Uhler <ruhler@csail.mit.edu>
2012-12-01 16:40:48 -0500

    Added argument pushing specialization.

--- seri/Seri/SMT/Specialize.hs
+++ seri/Seri/SMT/Specialize.hs
@@ -1,24 +1,64 @@
 
+{-# LANGUAGE PatternGuards #-}
+
 module Seri.SMT.Specialize (
     Logic(..), core,
     specialize,
     ) where
 
+import Data.List (genericLength)
+
+import Seri.Name
+import Seri.Type
+import Seri.Sig
+import Seri.Ppr
 import Seri.ExpH
 
 data Logic = Logic {
     th_integer :: Bool,
-    th_bit :: Bool
+    th_bit :: Bool,
+    th_lambda :: Bool
 }
 
 core :: Logic
 core = Logic {
     th_integer = False,
-    th_bit = False
+    th_bit = False,
+    th_lambda = False
   }
     
 -- Specialize an expression for a given logic.
 -- TODO: actually perform specialization.
 specialize :: Logic -> ExpH -> ExpH
-specialize _ = id
+specialize l e =
+ let me = specialize l
+ in case () of
+     _ | not (th_lambda l)
+       , Just (f@(CaseEH a k y n), arg) <- de_appEH e -> me $ pusharg f arg
+       | otherwise -> e
+
+-- Perform argument pushing.
+-- (case a of
+--     k -> y
+--     _ -> n) arg
+-- Where y = \v1 -> \v2 -> ... -> yv
+-- Translates to:
+--     case a of
+--         k -> \v1 -> \v2 -> ... -> yv arg
+--         _ -> n arg
+pusharg :: ExpH -> ExpH -> ExpH
+pusharg (CaseEH a k y n) arg =
+ let yify :: Integer -> (ExpH -> ExpH) -> ExpH -> ExpH
+     yify 0 f x = f x
+     yify n f (LamEH s b) = lamEH s $ \x -> yify (n-1) f (b x)
+     yify n f x = error $ "yify got: " ++ pretty x
+
+     kargs = genericLength (de_arrowsT (typeof k)) - 1
+
+     lam = lamEH (Sig (name "_z") (typeof arg)) $ \av ->
+        let ybody = \yv -> appEH yv av
+            y' = yify kargs ybody y
+            n' = appEH n av
+        in caseEH a k y' n'
+  in appEH lam arg
 
