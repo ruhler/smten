1212750dd5b311b4f94e99cc5d2b39a00e83da34
Richard Uhler <ruhler@csail.mit.edu>
2013-01-12 17:41:00 -0500

    Minor code cleanup.
    Changed naming scheme from: matchfoo to fooM for internal match functions.

--- seri/Seri/Exp/Match.hs
+++ seri/Seri/Exp/Match.hs
@@ -45,21 +45,21 @@ tupleP ps = ConP (tupleN (length ps)) ps
 -- case x of
 --     p -> yv
 --     _ -> n
-matchpatE :: (Fresh f) => Exp -> Pat -> Exp -> Exp -> f Exp
-matchpatE _ WildP yv _ = return yv
-matchpatE x (VarP n) yv _ = return $ appE (lamE (Sig n UnknownT) yv) x
-matchpatE x (AsP nm p) yv n = do
-    rest <- matchpatE x p yv n
+patM :: (Fresh f) => Exp -> Pat -> Exp -> Exp -> f Exp
+patM _ WildP yv _ = return yv
+patM x (VarP n) yv _ = return $ appE (lamE (Sig n UnknownT) yv) x
+patM x (AsP nm p) yv n = do
+    rest <- patM x p yv n
     return $ letE (Sig nm (typeof x)) x rest
-matchpatE x (LitP e) yv n =
+patM x (LitP e) yv n =
   let p = appsE (varE (Sig (name "==") UnknownT)) [e, x]
   in return $ ifE p yv n
-matchpatE x (ConP nm ps) yv n | isSimple n = do
-      y <- clauseE' [simpleMA ps yv] n
+patM x (ConP nm ps) yv n | isSimple n = do
+      y <- clauseM [simpleMA ps yv] n
       return $ CaseE x (Sig nm UnknownT) y n
-matchpatE x p y n = do
+patM x p y n = do
   nv <- fresh (Sig (name "_n") UnknownT)
-  body <- matchpatE x p y (varE nv)
+  body <- patM x p y (varE nv)
   return $ letE nv n body
 
 data Guard = PatG Pat Exp
@@ -70,31 +70,31 @@ data Guard = PatG Pat Exp
 -- | Perform a guard match
 --   | g = y
 --   | otherwise = n
-matchguardE :: (Fresh f) => Guard -> Exp -> Exp -> f Exp
-matchguardE (PatG p x) y n = matchpatE x p y n
-matchguardE (LetG decls) y _ = return (mletsE decls y)
-matchguardE (BoolG x) y n = return (ifE x y n)
+guardM :: (Fresh f) => Guard -> Exp -> Exp -> f Exp
+guardM (PatG p x) y n = patM x p y n
+guardM (LetG decls) y _ = return (mletsE decls y)
+guardM (BoolG x) y n = return (ifE x y n)
 
 -- | Perform multiple guard matches
 --   | g1, g2, ... = y
 --   | otherwise = n
-matchguardsE :: (Fresh f) => [Guard] -> Exp -> Exp -> f Exp
-matchguardsE [] y _ = return y 
-matchguardsE (g:gs) y n = do
-    y' <- matchguardsE gs y n
-    matchguardE g y' n
+guardsM :: (Fresh f) => [Guard] -> Exp -> Exp -> f Exp
+guardsM [] y _ = return y 
+guardsM (g:gs) y n = do
+    y' <- guardsM gs y n
+    guardM g y' n
 
 data Body = Body [Guard] Exp
     deriving (Eq, Show)
 
-matchbodyE :: (Fresh f) => Body -> Exp -> f Exp
-matchbodyE (Body gs y) n = matchguardsE gs y n
+bodyM :: (Fresh f) => Body -> Exp -> f Exp
+bodyM (Body gs y) n = guardsM gs y n
 
-matchbodiesE :: (Fresh f) => [Body] -> Exp -> f Exp
-matchbodiesE [] n = return n
-matchbodiesE (b:bs) n = do
-    n' <- matchbodiesE bs n
-    matchbodyE b n'
+bodiesM :: (Fresh f) => [Body] -> Exp -> f Exp
+bodiesM [] n = return n
+bodiesM (b:bs) n = do
+    n' <- bodiesM bs n
+    bodyM b n'
 
 data Alt = Alt Pat [Body]
     deriving (Eq, Show)
@@ -106,10 +106,10 @@ simpleA p e = Alt p [Body [] e]
 --  case x of
 --    alt 
 --    _ -> n
-matchaltE :: (Fresh f) => Exp -> Alt -> Exp -> f Exp
-matchaltE x (Alt p bs) n = do
-    body <- matchbodiesE bs n
-    matchpatE x p body n
+altM :: (Fresh f) => Exp -> Alt -> Exp -> f Exp
+altM x (Alt p bs) n = do
+    body <- bodiesM bs n
+    patM x p body n
 
 -- Match multiple alternatives:
 --   case x of
@@ -117,14 +117,14 @@ matchaltE x (Alt p bs) n = do
 --     alt2
 --     ...
 --     _ -> n
-matchaltsE :: (Fresh f) => Exp -> [Alt] -> Exp -> f Exp
-matchaltsE _ [] n = return n
-matchaltsE x (a:as) n | isSimple x = do
-    n' <- matchaltsE x as n
-    matchaltE x a n'
-matchaltsE x as n = do
+altsM :: (Fresh f) => Exp -> [Alt] -> Exp -> f Exp
+altsM _ [] n = return n
+altsM x (a:as) n | isSimple x = do
+    n' <- altsM x as n
+    altM x a n'
+altsM x as n = do
     xv <- fresh (Sig (name "_x") UnknownT)
-    body <- matchaltsE (varE xv) as n
+    body <- altsM (varE xv) as n
     return $ letE xv x body
 
 data MAlt = MAlt [Pat] [Body]
@@ -134,8 +134,8 @@ simpleMA :: [Pat] -> Exp -> MAlt
 simpleMA ps e = MAlt ps [Body [] e]
 
 -- Match a multi-argument alternative
-mmatchaltE :: (Fresh f) => [Sig] -> MAlt -> Exp -> f Exp
-mmatchaltE args (MAlt ps b) n = do
+maltM :: (Fresh f) => [Sig] -> MAlt -> Exp -> f Exp
+maltM args (MAlt ps b) n = do
   let -- case a b c of
       --    pa pb pc -> yv
       --    _ -> n
@@ -150,29 +150,29 @@ mmatchaltE args (MAlt ps b) n = do
       --                _ -> n
       --        _ -> n
       mkcases :: (Fresh m) => [(Pat, Exp)] -> [Body] -> Exp -> m Exp
-      mkcases [] bs n = matchbodiesE bs n
+      mkcases [] bs n = bodiesM bs n
       mkcases ((p, x):ps) y n = do
         body <- mkcases ps y n
-        matchpatE x p body n
+        patM x p body n
   mkcases (zip ps (map varE args)) b n
 
 -- Match multiple multi-argument alternatives
-mmatchaltsE :: (Fresh f) => [Sig] -> [MAlt] -> Exp -> f Exp
-mmatchaltsE args [] n = return n
-mmatchaltsE args (m:ms) n = do
-    n' <- mmatchaltsE args ms n
-    mmatchaltE args m n'
+maltsM :: (Fresh f) => [Sig] -> [MAlt] -> Exp -> f Exp
+maltsM args [] n = return n
+maltsM args (m:ms) n = do
+    n' <- maltsM args ms n
+    maltM args m n'
 
 -- | Desugar a case expression.
 mcaseE :: Exp -> [Alt] -> Exp
 mcaseE x alts
- = runFreshPretty $ matchaltsE x alts (errorE "case no match")
+ = runFreshPretty $ altsM x alts (errorE "case no match")
 
 clauseE :: [MAlt] -> Exp
-clauseE ms = runFreshPretty $ clauseE' ms (errorE "case no match")
+clauseE ms = runFreshPretty $ clauseM ms (errorE "case no match")
 
-clauseE' :: (Fresh f) => [MAlt] -> Exp -> f Exp
-clauseE' [MAlt ps e] n = do
+clauseM :: (Fresh f) => [MAlt] -> Exp -> f Exp
+clauseM [MAlt ps e] n = do
   -- If we are only making one match, we pick the variables for the lambda
   -- more wisely to avoid silly things like:
   --    \_p1 -> let a = _p1
@@ -187,12 +187,12 @@ clauseE' [MAlt ps e] n = do
         return (s, p)
   pvs <- mapM mkvar ps
   let (vars, ps') = unzip pvs
-  b <- mmatchaltE vars (MAlt ps' e) n
+  b <- maltM vars (MAlt ps' e) n
   return $ lamsE vars b
 
-clauseE' ms@(MAlt ps _ : _) n = do
+clauseM ms@(MAlt ps _ : _) n = do
     vars <- mapM fresh [Sig (name $ "_p" ++ show i) UnknownT | i <- [1..(length ps)]]
-    b <- mmatchaltsE vars ms n
+    b <- maltsM vars ms n
     return $ lamsE vars b
 
 -- | Lambda with pattern matching.
