e78645a376c71dcef812a6e42170199065f52747
Richard Uhler <ruhler@csail.mit.edu>
2013-02-05 13:55:51 -0500

    Pretty print (Foo a -> Foo b) instead of ((Foo a) -> (Foo b)).

--- seri/Seri/Type/Ppr.hs
+++ seri/Seri/Type/Ppr.hs
@@ -12,24 +12,30 @@ instance Ppr NType where
     ppr (VarNT v) = ppr v
     ppr (AppNT o a b) = parens (ppr a <+> text o <+> ppr b)
 
--- Print an atomic type.
--- Wraps complex types in parens.
-atom :: Type -> Doc
-atom t | Just _ <- de_listT t = ppr t
-atom t | Just _ <- de_tupleT t = ppr t
-atom t | Just _ <- de_arrowT t = parens (ppr t)
-atom t | Just _ <- de_appT t = parens (ppr t)
-atom t = ppr t
+-- Print an atomic type in an App context
+atomApp :: Type -> Doc
+atomApp t | Just _ <- de_listT t = ppr t
+atomApp t | Just _ <- de_tupleT t = ppr t
+atomApp t | Just _ <- de_arrowT t = parens (ppr t)
+atomApp t | Just _ <- de_appT t = parens (ppr t)
+atomApp t = ppr t
+
+-- Print an atomic type in an Arrow context
+atomArrow :: Type -> Doc
+atomArrow t | Just _ <- de_listT t = ppr t
+atomArrow t | Just _ <- de_tupleT t = ppr t
+atomArrow t | Just _ <- de_arrowT t = parens (ppr t)
+atomArrow t = ppr t
 
 instance Ppr Type where
     ppr t | Just v <- de_listT t = text "[" <> ppr v <> text "]"
     ppr t | Just vs <- de_tupleT t
       = text "(" <> hsep (punctuate comma (map ppr vs)) <> text ")"
     ppr t | (t1:ts@(_:_)) <- de_arrowsT t   
-      = hsep $ atom t1 : concat [[text "->", atom tx] | tx <- ts]
+      = hsep $ atomArrow t1 : concat [[text "->", atomArrow tx] | tx <- ts]
 
     ppr (ConT n) = ppr n
-    ppr (AppT a b) = atom a <+> atom b
+    ppr (AppT a b) = atomApp a <+> atomApp b
     ppr (VarT n) = ppr n
     ppr (NumT nt) = text "#" <> ppr nt
     ppr UnknownT = text "?"
