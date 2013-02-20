8ea601386202e847a26e5910bc8af074ca3ab94e
Richard Uhler <ruhler@csail.mit.edu>
2013-02-19 20:19:20 -0500

    Pull common DSEL functions into HasekllF.SMT.

--- smten-bin/dsel.hs
+++ smten-bin/dsel.hs
@@ -12,6 +12,7 @@ import Smten.SMT.Symbolic
 import Smten.SMT.SMT
 import Smten.HaskellF.HaskellF
 import Smten.HaskellF.TH
+import Smten.HaskellF.SMT
 import qualified Smten.HaskellF.Lib.Prelude as S
 import qualified Smten.HaskellF.Lib.Symbolic as S
 import qualified Smten_DSEL as S
@@ -21,26 +22,8 @@ import Smten.SMT.Yices.Yices2
 test :: (Monad m) => String -> Bool -> m ()
 test msg p = if p then return () else error msg
 
-symbolicHF :: (HaskellF a) => S.Symbolic a -> Symbolic a
-symbolicHF s 
- | Just v <- de_symbolicEH (unbox s) = box <$> v
- | otherwise = error "symbolicHF failed"
-
 freeHF :: (S.Free a, HaskellF a) => Symbolic a
 freeHF = symbolicHF S.free
-
-assertHF :: S.Bool -> Symbolic ()
-assertHF x = symbolicHF (S.assert x) >> return ()
-
-queryHF :: (HaskellF a) => Symbolic a -> SMT (Maybe a)
-queryHF s = do
-    r <- query (realize . unbox <$> s)
-    return (box <$> r)
-
-queryHFC :: (SmtenHF c f) => Symbolic f -> SMT (Maybe c)
-queryHFC s = do
-    r <- queryHF s
-    return (fromMaybe (error "queryHFC") . de_smtenHF <$> r)
 
 q1 :: SMT ()
 q1 = do
@@ -51,9 +34,23 @@ q1 = do
         return (x :: S.Integer)
     test "q1" (r == Just (5 :: Integer))
 
+incr :: S.Integer -> S.Integer
+incr x = x S.+ 1
+
+q2 :: SMT ()
+q2 = do
+    r <- queryHFC $ do
+        x <- freeHF
+        assertHF (x S.< 6)
+        assertHF (incr x S.> 5)
+        return (x :: S.Integer)
+    test "q2" (r == Just (5 :: Integer))
+
+
 qall :: SMT ()
 qall = do
     q1
+    q2
 
 main :: IO ()
 main = do
---
+++ smten-smt/Smten/HaskellF/SMT.hs
@@ -0,0 +1,34 @@
+
+{-# LANGUAGE PatternGuards #-}
+
+module Smten.HaskellF.SMT (
+    symbolicHF, assertHF, queryHF, queryHFC,
+    ) where
+
+import Data.Functor((<$>))
+import Data.Maybe (fromMaybe)
+
+import Smten.SMT.Symbolic
+import Smten.SMT.SMT
+import Smten.HaskellF.HaskellF
+import qualified Smten.HaskellF.Lib.Prelude as S
+import qualified Smten.HaskellF.Lib.Symbolic as S
+
+
+symbolicHF :: (HaskellF a) => S.Symbolic a -> Symbolic a
+symbolicHF s 
+ | Just v <- de_symbolicEH (unbox s) = box <$> v
+ | otherwise = error "symbolicHF failed"
+
+assertHF :: S.Bool -> Symbolic ()
+assertHF x = symbolicHF (S.assert x) >> return ()
+
+queryHF :: (HaskellF a) => Symbolic a -> SMT (Maybe a)
+queryHF s = do
+    r <- query (realize . unbox <$> s)
+    return (box <$> r)
+
+queryHFC :: (SmtenHF c f) => Symbolic f -> SMT (Maybe c)
+queryHFC s = do
+    r <- queryHF s
+    return (fromMaybe (error "queryHFC") . de_smtenHF <$> r)
