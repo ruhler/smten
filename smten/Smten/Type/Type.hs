84f709d253d7d2ed8613e2aa722fed26b4f292c8
Richard Uhler <ruhler@csail.mit.edu>
2013-04-03 08:07:06 -0400

    Implement Ord for Type manually.
    Because Ord should not depend on the Kind.

--- smten/Smten/Type/Type.hs
+++ smten/Smten/Type/Type.hs
@@ -22,10 +22,9 @@ data Type = ConT Name Kind                  -- ^ type constructor
           | NumT Integer                    -- ^ numeric type constructor
           | OpT NTOp Type Type              -- ^ numeric type operation
           | UnknownT
-      deriving(Ord, Show)
+      deriving(Show)
 
 -- Kinds don't affect equality of type
--- TODO: Instance of Ord for Type should not look at kind!
 instance Eq Type where
     (==) (ConT a _) (ConT b _) = a == b
     (==) (AppT a1 a2) (AppT b1 b2) = and [a1 == b1, a2 == b2]
@@ -34,6 +33,30 @@ instance Eq Type where
     (==) (OpT a1 a2 a3) (OpT b1 b2 b3) = and [a1 == b1, a2 == b2, a3 == b3]
     (==) UnknownT UnknownT = True
     (==) _ _ = False
+
+instance Ord Type where
+    compare (ConT a _) (ConT b _) = compare a b
+    compare (ConT {}) _ = LT
+    compare _ (ConT {}) = GT
+
+    compare (AppT a1 a2) (AppT b1 b2) = compare (a1, a2) (b1, b2)
+    compare (AppT {}) _ = LT
+    compare _ (AppT {}) = GT
+
+    compare (VarT a _) (VarT b _) = compare a b 
+    compare (VarT {}) _ = LT
+    compare _ (VarT {}) = GT
+
+    compare (NumT a) (NumT b) = compare a b
+    compare (NumT {}) _ = LT
+    compare _ (NumT {}) = GT
+
+    compare (OpT a1 a2 a3) (OpT b1 b2 b3) = compare (a1, a2, a3) (b1, b2, b3)
+    compare (OpT {}) _ = LT
+    compare _ (OpT {}) = GT
+    
+    compare UnknownT UnknownT = EQ
+    
 
 -- | Evaluate a concrete numeric type.
 nteval :: Type -> Integer
