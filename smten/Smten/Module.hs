4ed5d5469b7a2543c8f6510260cfcdbe51b924fc
Richard Uhler <ruhler@csail.mit.edu>
2013-04-08 16:05:09 -0400

    Pulled qualify code out of Module.Module into its own module.

--- smten/Smten/Module.hs
+++ smten/Smten/Module.hs
@@ -1,9 +1,11 @@
 
 module Smten.Module (
     module Smten.Module.Module,
+    module Smten.Module.Qualify,
     module Smten.Module.Ppr
     ) where
 
 import Smten.Module.Module
+import Smten.Module.Qualify
 import Smten.Module.Ppr
 
--- smten/Smten/Module/Module.hs
+++ smten/Smten/Module/Module.hs
@@ -34,36 +34,20 @@
 -------------------------------------------------------------------------------
 
 {-# LANGUAGE PatternGuards #-}
-{-# LANGUAGE TypeSynonymInstances #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
 
 module Smten.Module.Module (
     Module(..), Import(..), Synonym(..), DataDec(..), Deriving(..),
-    sderive, flatten, flatten1,
+    sderive,
     ) where
 
-import Control.Monad.Error
-import Control.Monad.Writer hiding ((<>))
-import Control.Monad.Reader
-
-import qualified Data.HashSet as Set
-import Data.Functor ((<$>))
-import Data.List(nub)
-import Data.Maybe(fromMaybe, catMaybes)
-
 import qualified Smten.HashTable as HT
+
 import Smten.Failable
-import Smten.Ppr
 import Smten.Location
 import Smten.Name
-import Smten.Sig
 import Smten.Type
-import Smten.Exp
 import Smten.Dec
 
-
--- TODO: support import specs.
 data Import = Import { 
     imp_from :: Name,
     imp_as :: Name,
@@ -102,238 +86,8 @@ data Module = Module {
     mod_decs :: [Dec]
 } deriving(Show, Eq)
 
-data QS = QS {
-    qs_env :: [Module],     -- ^ The environment
-    qs_me :: Module,        -- ^ The current module
-    qs_bound :: [Name],     -- ^ List of bound variable names
-    qs_syns :: HT.HashTable Name ([Name], Type), -- ^ All type synonyms
-    qs_exports :: HT.HashTable Name (Set.Set Name),      -- ^ Module exports
-    qs_loc :: Location      -- ^ The current location
-}
-
-type QualifyM = ReaderT QS Failable
-
-instance MonadErrorSL QualifyM where
-    errloc = asks qs_loc
-
-withloc :: Location -> QualifyM a -> QualifyM a
-withloc l = local (\r -> r { qs_loc = l } )
-
-mkSyns :: [Synonym] -> HT.HashTable Name ([Name], Type)
-mkSyns xs = HT.table [(n, (vs, t)) | Synonym n vs t <- xs]
-
 mkDDecs :: [DataDec] -> HT.HashTable Name [ConRec]
 mkDDecs xs = HT.table [(n, cs) | DataDec n _ cs <- xs]
-
-mkExports :: [Module] -> HT.HashTable Name (Set.Set Name)
-mkExports xs = HT.table [(mod_name m, exports m) | m <- xs]
-
-mename :: QualifyM Name
-mename = asks (mod_name . qs_me)
-
-withbound :: [Name] -> QualifyM a -> QualifyM a
-withbound binds = local (\qs -> qs { qs_bound = binds ++ qs_bound qs })
-
-isbound :: Name -> QualifyM Bool
-isbound n = do
-    bound <- asks qs_bound
-    return $ n `elem` bound
-
-class Qualify a where
-    -- | Resolve all unqualified names in the given object.
-    qualify :: a -> QualifyM a 
-
-instance Qualify Module where
-    qualify m = local (\qs -> qs { qs_me = m }) $ do
-        sy' <- mapM qualify (mod_synonyms m)
-        ds' <- mapM qualify (mod_decs m)
-        return $ m { mod_synonyms = sy', mod_decs = ds' }
-
-instance Qualify Synonym where
-    qualify (Synonym n vs t) = Synonym n vs <$> qualify t
-
-qdefine :: Name -> QualifyM Name
-qdefine n 
- | isqualified n = return n
- | otherwise = do
-    menm <- mename
-    return $ qualified menm n
-
-instance Qualify TopSig where
-    qualify (TopSig nm ctx t) = do
-        nm' <- qdefine nm
-        ctx' <- qualify ctx
-        t' <- qualify t
-        return (TopSig nm' ctx' t')
-
-instance (Qualify a) => Qualify [a] where
-    qualify = mapM qualify
-
-instance Qualify Class where
-    qualify (Class nm ts) = do
-        nm' <- qualify nm
-        ts' <- qualify ts
-        return $ Class nm' ts'
-
-instance Qualify Con where
-    qualify (Con nm ts) = do
-        nm' <- qdefine nm
-        ts' <- qualify ts
-        return $ Con nm' ts'
-
-instance Qualify TopExp where
-    qualify (TopExp ts body) = do
-           ts' <- qualify ts
-           body' <- qualify body
-           return (TopExp ts' body')
-
-instance Qualify Dec where
-    qualify d@(ValD l e) = withloc l $ ValD l <$> qualify e
-
-    qualify (DataD l n vars cs) = withloc l $ do
-        n' <- qdefine n
-        cs' <- qualify cs
-        return $ DataD l n' vars cs'
-
-    qualify (ClassD l ctx nm vars sigs) = withloc l $ do
-        nm' <- qdefine nm
-        ctx' <- qualify ctx
-        sigs' <- mapM qualify sigs
-        return (ClassD l ctx' nm' vars sigs')
-
-    qualify (InstD l ctx cls meths) = withloc l $ do
-        ctx' <- qualify ctx
-        cls' <- qualify cls
-        meths' <- mapM qualify meths
-        return (InstD l ctx' cls' meths')
-
-    qualify (PrimD l ts) = withloc l $ do
-        ts' <- qualify ts
-        return (PrimD l ts')
-
-instance Qualify Type where
-    qualify t = do
-        syns <- asks qs_syns
-        case t of
-          t | (ConT nm _, args) <- de_appsT t
-            , Just (vs, t') <- HT.lookup nm syns ->
-                if length vs > length args
-                    then lthrow $ "expecting at least "
-                             ++ show (length vs)
-                             ++ " argument(s) to synonym "
-                             ++ pretty nm ++ " in " ++ pretty t
-                    else let (bound, rest) = splitAt (length vs) args
-                         in qualify (appsT (assign (zip vs bound) t') rest)
-          ConT n k -> do
-            n' <- qualify n
-            return (ConT n' k)
-          AppT a b -> do
-            a' <- qualify a
-            b' <- qualify b
-            return (AppT a' b')
-          VarT {} -> return t
-          NumT {} -> return t
-          OpT {} -> return t
-          UnknownT {} -> return t
-
-instance Qualify Exp where
-    qualify e@(LitE {}) = return e
-    qualify (ConE l (Sig n t)) = withloc l $ do
-        n' <- qualify n
-        t' <- qualify t
-        return (ConE l (Sig n' t'))
-    qualify (VarE l (Sig n t)) = withloc l $ do
-        t' <- qualify t
-        bound <- isbound n
-        if bound 
-            then return (VarE l (Sig n t'))
-            else do
-                n' <- resolve n
-                return (VarE l (Sig n' t'))
-    qualify (AppE l f x) = withloc l $ do
-        f' <- qualify f
-        x' <- qualify x
-        return (AppE l f' x')
-    qualify (LamE l (Sig n t) b) = withloc l $ do
-        t' <- qualify t
-        LamE l (Sig n t') <$> (withbound [n] $ qualify b)
-    
-    qualify (CaseE l x (Sig kn kt) y n) = withloc l $ do
-        kn' <- qualify kn
-        kt' <- qualify kt
-        x' <- qualify x
-        y' <- qualify y
-        n' <- qualify n
-        return $ CaseE l x' (Sig kn' kt') y' n'
-
-instance Qualify Method where
-    qualify (Method nm e) = do
-        e' <- qualify e
-        nm' <- qualify nm
-        return (Method nm' e')
-
-instance Qualify Name where
-    qualify = resolve
-
--- Return the set of entities exported by the given module.
-exports :: Module -> (Set.Set Name)
-exports m =
-  let exdec :: Dec -> Writer [Name] ()
-      exdec (ValD _ (TopExp (TopSig nm _ _) _)) = tell [unqualified nm]
-      exdec (DataD _ nm _ cs) = tell [unqualified nm] >> mapM_ excon cs
-      exdec (ClassD _ _ nm _ sigs) = tell [unqualified nm] >> mapM_ exmeth sigs
-      exdec (InstD {}) = return ()
-      exdec (PrimD _ (TopSig nm _ _)) = tell [unqualified nm]
-
-      exmeth :: TopExp -> Writer [Name] ()
-      exmeth (TopExp (TopSig snm _ _) _) = tell [unqualified snm]
-       
-      excon :: Con -> Writer [Name] ()
-      excon (Con n _) = tell [unqualified n]
-    
-  in Set.fromList $ execWriter (mapM exdec (mod_decs m))
-
--- Resolve the given name based on the given import.
--- Returns the unique name for the entity if it is accessible via this import.
-resolvein :: Name -> Import -> QualifyM (Maybe Name)
-resolvein n (Import fr as qo) = do
-  exps <- asks qs_exports
-  case HT.lookup fr exps of
-    Just xs -> do
-      let uqn = unqualified n
-          qn = qualification n
-      return $ do
-          guard $ uqn `Set.member` xs
-          guard $ qn == as || (not qo && nnull qn)
-          return $ qualified fr uqn
-    Nothing -> lthrow $ "Module " ++ pretty fr ++ " not found"
-        
--- | Return the unique name for the entity referred to by the given name.
-resolve :: Name -> QualifyM Name
-resolve n = do
-  me <- asks qs_me
-  let meimport = Import (mod_name me) (mod_name me) False
-  finds <- mapM (resolvein n) (meimport : mod_imports me)
-  case nub $ catMaybes finds of
-      [] -> lthrow $ "'" ++ pretty n ++ "' not found in module " ++ pretty (mod_name me)
-      [x] -> return x
-      xs -> lthrow $ "'" ++ pretty n ++ "' is ambiguous: " ++ show xs
-
--- | Flatten a complete module hierarchy.
-flatten :: [Module] -> Failable [Dec]
-flatten ms = do
-    ds <- mapM (flatten1 ms) ms
-    return $ concat ds
-
--- | Flatten a single module.
--- Assumes standalone deriving has already been performed.
-flatten1 :: [Module]    -- ^ The environment
-            -> Module   -- ^ The module to flatten
-            -> Failable [Dec] -- ^ Flattened declarations from the module
-flatten1 ms m = do
-  let syns = mkSyns $ concatMap mod_synonyms ms
-      exps = mkExports ms
-  mod_decs <$> runReaderT (qualify m) (QS ms (error "not in module") [] syns exps lunknown)
 
 -- Perform standalone derivings in the given module.
 sderive :: [Module] -> Module -> Failable Module
