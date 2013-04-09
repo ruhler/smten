5971386f3ece3e8935195d21e256eb0bf7e43054
Richard Uhler <ruhler@csail.mit.edu>
2013-04-09 13:43:49 -0400

    Implemented hsqName function.
    Not used yet.

--- smten/Smten/HaskellF/Compile/Name.hs
+++ smten/Smten/HaskellF/Compile/Name.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE PatternGuards #-}
 
 module Smten.HaskellF.Compile.Name (
-    hsName, hsTyName, nmn, nmk,
+    hsName, hsqName, hsTyName, nmn, nmk,
     casenm, symnm, hfpre,
     ) where
 
@@ -15,32 +15,29 @@ import Smten.Type
 
 import Smten.HaskellF.Compile.Kind
 
--- | Translate a data constructor or variable name to haskell.
+-- | Translate a data constructor or variable name to an unqualified haskell
+-- name.
 -- Remaps builtin prelude data constructor names as appropriate.
 -- Adds parenthesis around operator names as appropriate.
---
--- TODO: Here we just drop the qualified part of the name.
--- This is a hack, requiring there are no modules which define an entity of
--- the same name (unlikely...). Really we should form a proper haskell name
--- for whatever this name is used for (varid, conid)
 hsName :: Name -> H.Name
 hsName n =
-  let dequalify :: String -> String
-      dequalify n = 
-        case break (== '.') n of
-            (n', []) -> n'
-            (_, ".") -> "."
-            (_, n') -> dequalify (tail n')
-      symify :: String -> String
+  let symify :: String -> String
       symify s = if issymbol s then "(" ++ s ++ ")" else s
-  in H.mkName . symify . dequalify . hfnm $ n
+  in H.mkName . symify . hfnm $ n
+
+-- Same as hsName, only produces the qualified version of the name.
+hsqName :: Name -> H.Name
+hsqName n =
+  let symify :: String -> String
+      symify s = if issymbol (hfnm n) then "(" ++ s ++ ")" else s
+  in H.mkName . symify . hfqnm $ n
 
 issymbol :: String -> Bool
 issymbol ('(':_) = False
 issymbol "[]" = False
 issymbol (h:_) = not $ isAlphaNum h || h == '_'
 
--- | Translate a type constructor name to haskell.
+-- | Translate a type constructor name to an unqualified haskell name.
 -- Remaps builtin prelude type constructor names as appropriate.
 hsTyName :: Name -> H.Name
 hsTyName n = hsName $ name (hftynm n)
@@ -58,8 +55,8 @@ nmn s n = H.mkName $ s ++ show n
 nmk :: String -> Kind -> H.Name
 nmk s k = nmn s (knum k)
 
--- Convert a Smten data constructor name to it's corresponding HaskellF data
--- constructor name.
+-- Convert a Smten data constructor name to it's corresponding unqualified
+-- HaskellF data constructor name.
 hfnm :: Name -> String
 hfnm n
  | Just i <- de_tupleN n = "Tuple" ++ show i ++ "__"
@@ -68,8 +65,19 @@ hfnm n
  | n == nilN = "Nil__"
  | otherwise = unname (unqualified n)
 
--- Convert a Smten type constructor name to it's corresponding HaskellF type
--- constructor name.
+-- Convert a Smten data constructor name to it's corresponding qualified
+-- HaskellF data constructor name.
+hfqnm :: Name -> String
+hfqnm n
+ | Just i <- de_tupleN n = "S.Tuple" ++ show i ++ "__"
+ | n == unitN = "S.Unit__"
+ | n == consN = "S.Cons__"
+ | n == nilN = "S.Nil__"
+ | isqualified n = unname (hfpre n)
+ | otherwise = unname n
+
+-- Convert a Smten type constructor name to it's corresponding unqualified
+-- HaskellF type constructor name.
 hftynm :: Name -> String
 hftynm n
  | n == unitN = "Unit__"
@@ -77,12 +85,12 @@ hftynm n
  | n == nilN = "List__"
  | otherwise = unname (unqualified n)
 
--- Given the name of a data constructor, return the name of the function for
--- doing a case match against the constructor.
+-- Given the name of a data constructor, return the unqualified name of the
+-- function for doing a case match against the constructor.
 casenm :: Name -> H.Name
 casenm n = H.mkName $ "__case" ++ hfnm n
 
--- | Given the name of a type constructor, return the symbolic data
+-- | Given the name of a type constructor, return the unqualified symbolic data
 -- constructor associated with it.
 symnm :: Name -> H.Name
 symnm n = H.mkName $ hftynm n ++ "__s"
