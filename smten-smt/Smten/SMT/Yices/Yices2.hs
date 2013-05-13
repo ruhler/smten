4aa8a847f6ce1070d3c4b16e6b9f5c888d707702
Richard Uhler <ruhler@csail.mit.edu>
2013-05-13 11:52:23 -0400

    Replaced Yices2 backend entirely with Yices2b.

--- smten-smt/Smten/SMT/Yices/Yices2.hs
+++ smten-smt/Smten/SMT/Yices/Yices2.hs
@@ -33,27 +33,28 @@
 -- 
 -------------------------------------------------------------------------------
 
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE PatternGuards #-}
 
 -- | Backend for the Yices2 solver
 module Smten.SMT.Yices.Yices2 (yices2) where
-
-import qualified Data.Map as Map
-import Data.List(genericLength)
 
 import Foreign
 import Foreign.C.String
 import Foreign.C.Types
 
 import Smten.SMT.Yices.FFI2
-import Smten.SMT.Syntax hiding (Type(..))
-import Smten.SMT.Translate
-import qualified Smten.SMT.Yices.Concrete as YC
+import Smten.SMT.AST
+import qualified Smten.SMT.Assert as A
 import qualified Smten.SMT.Solver as S
 import Smten.Name
-import Smten.Type
 import Smten.Sig
+import Smten.Lit
+import Smten.Type
+import Smten.Bit
 import Smten.ExpH
+import qualified Smten.HashTable as HT
 
 data Yices2 = Yices2 (Ptr YContext)
 
@@ -70,7 +71,7 @@ yices2 = do
           S.push = push y2,
           S.pop = pop y2,
           S.declare = declare y2,
-          S.assert = assert y2,
+          S.assert = y2assert y2,
           S.check = check y2,
           S.getIntegerValue = getIntegerValue y2,
           S.getBoolValue = getBoolValue y2,
@@ -83,10 +84,8 @@ declare _ (Sig s ty) = do
     term <- c_yices_new_uninterpreted_term ty'
     withCString (unname s) $ c_yices_set_term_name term
 
-assert :: Yices2 -> ExpH -> IO ()
-assert (Yices2 yctx) p = do
-    p' <- yterm $ {-# SCC "TRANSLATE" #-} smtE (fromExpH p)
-    c_yices_assert_formula yctx p'
+y2assert :: Yices2 -> ExpH -> IO ()
+y2assert = A.assert
 
 push :: Yices2 -> IO ()
 push (Yices2 yctx) = c_yices_push yctx
@@ -103,7 +102,7 @@ getIntegerValue :: Yices2 -> Name -> IO Integer
 getIntegerValue (Yices2 yctx) nm = do
     model <- c_yices_get_model yctx 1
     x <- alloca $ \ptr -> do
-            term <- yterm (varE (unname nm))
+            term <- withCString (unname nm) c_yices_get_term_by_name
             ir <- c_yices_get_int64_value model term ptr
             if ir == 0
                then do 
@@ -117,7 +116,7 @@ getBoolValue :: Yices2 -> Name -> IO Bool
 getBoolValue (Yices2 yctx) nm = do
     model <- c_yices_get_model yctx 1
     x <- alloca $ \ptr -> do
-            term <- yterm (varE (unname nm))
+            term <- withCString (unname nm) c_yices_get_term_by_name
             ir <- c_yices_get_bool_value model term ptr
             case ir of
                _ | ir == (-1) -> do
@@ -140,7 +139,7 @@ getBitVectorValue :: Yices2 -> Integer -> Name -> IO Integer
 getBitVectorValue (Yices2 yctx) w nm = do
     model <- c_yices_get_model yctx 1
     bits <- allocaArray (fromInteger w) $ \ptr -> do
-        term <- yterm (varE (unname nm))
+        term <- withCString (unname nm) c_yices_get_term_by_name
         ir <- c_yices_get_bv_value model term ptr
         if ir == 0
             then peekArray (fromInteger w) ptr
@@ -148,17 +147,9 @@ getBitVectorValue (Yices2 yctx) w nm = do
     c_yices_free_model model
     return $! bvInteger bits
         
-                
-
 bvInteger :: [Int32] -> Integer
 bvInteger [] = 0
 bvInteger (x:xs) = bvInteger xs * 2 + (fromIntegral x)
-
-withstderr :: (Ptr CFile -> IO a) -> IO a
-withstderr f = do
-    cf <- withCString "w" $ \str -> c_fdopen 2 str
-    x <- f cf 
-    return $! x
 
 ytype :: Type -> IO YType
 ytype t
@@ -166,141 +157,46 @@ ytype t
  | t == integerT = c_yices_int_type
  | t == boolT = c_yices_bool_type
 
-isbinop :: String -> Expression -> Bool
-isbinop nm (AppE (VarE n) [_, _]) = n == nm
-isbinop _ _ = False
+instance AST Yices2 YTerm where
+  assert (Yices2 yctx) = c_yices_assert_formula yctx
 
-dobinop' :: Map.Map String YTerm -> Expression -> Expression -> (YTerm -> YTerm -> IO YTerm) -> IO YTerm
-dobinop' s a b f = do
-    at <- ytermS s a
-    bt <- ytermS s b
-    f at bt
+  literal _ l
+    | Just i <- de_integerL l = c_yices_int64 (fromInteger i)
+    | Just bv <- de_bitL l =
+        let w = fromInteger $ bv_width bv
+            v = fromInteger $ bv_value bv
+        in c_yices_bvconst_uint64 w v
 
-dobinop :: Map.Map String YTerm -> Expression -> (YTerm -> YTerm -> IO YTerm) -> IO YTerm
-dobinop s (AppE (VarE _) [a, b]) f = dobinop' s a b f
-
-yterm :: Expression -> IO YTerm
-yterm = ytermS Map.empty
-
-builtin :: [String]
-builtin = [
-    "true", "false", "if", "ite", "=", "/=", "distinct",
-    "or", "and", "not", "xor", "<=>", "=>", "mk-tuple",
-    "select", "tuple-update", "update",  "forall",  "exists",  "let",
-    "+", "-", "*", "/", "^",
-    "<", "<=", ">", ">=",
-    "mk-bv", "bv-add", "bv-sub", "bv-mul", "bv-neg", "bv-pow",
-    "bv-not", "bv-and", "bv-or", "bv-xor", "bv-nand", "bv-nor", "bv-xnor",
-    "bv-shift-left0", "bv-shift-left1", "bv-shift-right0", "bv-shift-right1",
-    "bv-ashift-right", "bv-rotate-left", "bv-rotate-right",
-    "bv-extract", "bv-concat", "bv-repeat",
-    "bv-sign-extend", "bv-zero-extend", "bv-ge", "bv-gt", "bv-le", "bv-lt",
-    "bv-sge", "bv-sgt", "bv-sle", "bv-slt", "bv-shl", "bv-lshr", "bv-ashr",
-    "bv-div", "bv-rem", "bv-sdiv", "bv-srem", "bv-smod",
-    "bv-redor", "bv-redand", "bv-comp"
+  bool _ p = if p then c_yices_true else c_yices_false
+  var _ nm = withCString (unname nm) c_yices_get_term_by_name
+  ite _ = c_yices_ite 
+  unary = HT.table [(name "Smten.Bit.__prim_not_Bit", const c_yices_bvnot)]
+  binary = HT.table [
+    (name "Prelude.__prim_eq_Integer", const c_yices_eq),
+    (name "Prelude.__prim_add_Integer", const c_yices_add),
+    (name "Prelude.__prim_sub_Integer", const c_yices_sub),
+    (name "Prelude.__prim_mul_Integer", const c_yices_mul),
+    (name "Prelude.__prim_lt_Integer", const c_yices_arith_lt_atom),
+    (name "Prelude.__prim_leq_Integer", const c_yices_arith_leq_atom),
+    (name "Prelude.__prim_gt_Integer", const c_yices_arith_gt_atom),
+    (name "Prelude.__prim_geq_Integer", const c_yices_arith_geq_atom),
+    (name "Smten.Bit.__prim_eq_Bit", const c_yices_eq),
+    (name "Smten.Bit.__prim_lt_Bit", const c_yices_bvlt_atom),
+    (name "Smten.Bit.__prim_leq_Bit", const c_yices_bvle_atom),
+    (name "Smten.Bit.__prim_gt_Bit", const c_yices_bvgt_atom),
+    (name "Smten.Bit.__prim_geq_Bit", const c_yices_bvge_atom),
+    (name "Smten.Bit.__prim_add_Bit", const c_yices_bvadd),
+    (name "Smten.Bit.__prim_sub_Bit", const c_yices_bvsub),
+    (name "Smten.Bit.__prim_mul_Bit", const c_yices_bvmul),
+    (name "Smten.Bit.__prim_or_Bit", const c_yices_bvor),
+    (name "Smten.Bit.__prim_and_Bit", const c_yices_bvand),
+    (name "Smten.Bit.__prim_concat_Bit", const c_yices_bvconcat),
+    (name "Smten.Bit.__prim_shl_Bit", const c_yices_bvshl),
+    (name "Smten.Bit.__prim_lshr_Bit", const c_yices_bvlshr)
     ]
 
-
--- Construct a yices term from the given expression with the given
--- variable scope.
-ytermS :: Map.Map String YTerm -> Expression -> IO YTerm
-ytermS s e
-  | Just (a, b) <- de_eqE e = dobinop' s a b c_yices_eq
-  | Just [a, b] <- de_orE e = dobinop' s a b c_yices_or2
-  | Just [a, b] <- de_andE e = dobinop' s a b c_yices_and2
-  | Just (a, b) <- de_bvaddE e = dobinop' s a b c_yices_bvadd
-  | Just (a, b) <- de_bvsubE e = dobinop' s a b c_yices_bvsub
-  | Just (a, b) <- de_bvorE e = dobinop' s a b c_yices_bvor
-  | Just (a, b) <- de_bvandE e = dobinop' s a b c_yices_bvand
-  | Just (a, b) <- de_bvconcatE e = dobinop' s a b c_yices_bvconcat
-  | Just (a, b) <- de_addE e = dobinop' s a b c_yices_add
-  | Just (a, b) <- de_subE e = dobinop' s a b c_yices_sub
-  | Just (a, b) <- de_mulE e = dobinop' s a b c_yices_mul
-ytermS s e | isbinop "<" e = dobinop s e c_yices_arith_lt_atom
-ytermS s e | isbinop "<=" e = dobinop s e c_yices_arith_leq_atom
-ytermS s e | isbinop ">" e = dobinop s e c_yices_arith_gt_atom
-ytermS s e | isbinop ">=" e = dobinop s e c_yices_arith_geq_atom
-ytermS s e | isbinop "bv-lt" e = dobinop s e c_yices_bvlt_atom
-ytermS s e | isbinop "bv-le" e = dobinop s e c_yices_bvle_atom
-ytermS s e | isbinop "bv-gt" e = dobinop s e c_yices_bvgt_atom
-ytermS s e | isbinop "bv-ge" e = dobinop s e c_yices_bvge_atom
-ytermS s e | isbinop "xor" e = dobinop s e c_yices_xor2
-ytermS s e | isbinop "bv-sub" e = dobinop s e c_yices_bvsub
-ytermS s e | isbinop "bv-mul" e = dobinop s e c_yices_bvmul
-ytermS s e | isbinop "bv-shl" e = dobinop s e c_yices_bvshl
-ytermS s e | isbinop "bv-lshr" e = dobinop s e c_yices_bvlshr
-ytermS s e | Just a <- de_notE e = do
-    at <- ytermS s a
-    c_yices_not at
-ytermS s e | Just a <- de_bvnotE e = do
-    at <- ytermS s a
-    c_yices_bvnot at
-ytermS s (AppE (VarE "select") [v, LitE (IntegerL i)]) = do
-    vt <- ytermS s v
-    c_yices_select (fromInteger i) vt
-ytermS s e | Just (p, a, b) <- de_ifE e = do
-    pt <- ytermS s p 
-    at <- ytermS s a
-    bt <- ytermS s b
-    c_yices_ite pt at bt
-ytermS s (AppE (VarE "mk-tuple") args) = do
-    argst <- mapM (ytermS s) args
-    withArray argst $ c_yices_tuple (fromIntegral $ length argst)
-ytermS s e | Just (w, v) <- de_mkbvE e = do
-    c_yices_bvconst_uint64 (fromInteger w) (fromInteger v)
-ytermS s e | Just (a, n) <- de_bvzeroExtendE e = do
-    at <- ytermS s a
-    c_yices_zero_extend at (fromInteger n)
-ytermS s e | Just (a, n) <- de_bvsignExtendE e = do
-    at <- ytermS s a
-    c_yices_sign_extend at (fromInteger n)
-
--- syntax for bv-extract is: bv-extract end begin bv
--- for the c api, we have: bvextract bv begin end
-ytermS s (AppE (VarE "bv-extract") [LitE (IntegerL end), LitE (IntegerL begin), x]) = do
-    xt <- ytermS s x
-    c_yices_bvextract xt (fromInteger begin) (fromInteger end)
-
-ytermS s (AppE (VarE "and") args) = do
-    argst <- mapM (ytermS s) args
-    withArray argst $ c_yices_and (fromIntegral $ length argst)
-ytermS s (AppE (VarE "or") args) = do
-    argst <- mapM (ytermS s) args
-    withArray argst $ c_yices_or (fromIntegral $ length argst)
-ytermS s e@(AppE (VarE f) _) | f `elem` builtin = do
-    error $ "TODO: yterm builtin " ++ YC.pretty e
-ytermS s (AppE f [a]) = do
-    ft <- ytermS s f
-    at <- ytermS s a 
-    withArray [at] $ c_yices_application ft 1
-ytermS s e | Just (bs, v) <- de_letE e =
-  let mkvar :: (String, Expression) -> IO (String, YTerm)
-      mkvar (nm, e) = do
-        et <- ytermS s e
-        return (nm, et)
-  in do
-    vars <- mapM mkvar bs
-    ytermS (Map.union s (Map.fromList vars)) v
-        
-ytermS _ (LitE (BoolL True)) = c_yices_true
-ytermS _ (LitE (BoolL False)) = c_yices_false
-ytermS _ (LitE (IntegerL i)) = c_yices_int64 (fromInteger i)
-ytermS s e@(VarE nm) =
-    case Map.lookup nm s of
-        Nothing -> withCString nm c_yices_get_term_by_name
-        Just t -> return t 
-    
-ytermS _ e = error $ "TODO: yterm: " ++ YC.pretty e
-
-
--- | Construct a yices term for the given expression. This works by printing
--- the expression to a string and passing the string over to yices to parse.
-ytermbystr :: Expression -> IO YTerm
-ytermbystr e = do
-    ye <- withCString (YC.concrete e) $ \str -> c_yices_parse_term str
-    if ye < 0 
-        then do 
-            withstderr $ \stderr -> c_yices_print_error stderr
-            error $ "yterm error"
-        else return $! ye
+  zeroextend _ a' n = c_yices_zero_extend a' (fromInteger n)
+  signextend _ a' n = c_yices_sign_extend a' (fromInteger n)
+  extract _ x' begin end = c_yices_bvextract x' (fromInteger begin) (fromInteger end)
+  truncate _ x' w = c_yices_bvextract x' (fromInteger $ w-1) 0
 
