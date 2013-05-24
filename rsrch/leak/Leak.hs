55aa82ce88e10ce3d56e70af120365ac7012fdaf
Richard Uhler <ruhler@csail.mit.edu>
2013-05-24 17:34:12 -0400

    Made Leak totally stand alone with respect to HaskellF.

--- rsrch/leak/Leak.hs
+++ rsrch/leak/Leak.hs
@@ -1,19 +1,83 @@
-{-# LANGUAGE ExplicitForAll #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
+
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE InstanceSigs #-}
-{-# LANGUAGE KindSignatures #-}
 
 import Smten.Name
+import Smten.Sig
 import Smten.Type
 import Smten.ExpH
 import Smten.Prim
 
 import Prelude hiding (foldr, replicate, (&&), and, Bool(..), Integer) 
-import Smten.HaskellF.HaskellF
+
+class (SmtenT a) => HaskellF a where
+    box :: ExpH -> a
+    unbox :: a -> ExpH
+
+class (SmtenT1 m) => HaskellF1 m where
+    box1 :: (HaskellF a) => ExpH -> m a
+    unbox1 :: (HaskellF a) => m a -> ExpH 
+
+class (SmtenT2 m) => HaskellF2 m where
+    box2 :: (HaskellF a, HaskellF b) => ExpH -> m a b
+    unbox2 :: (HaskellF a, HaskellF b) => m a b -> ExpH
+
+instance (HaskellF1 m, HaskellF a) => HaskellF (m a) where
+    box = box1
+    unbox = unbox1
+
+instance (HaskellF2 m, HaskellF a) => HaskellF1 (m a) where
+    box1 = box2
+    unbox1 = unbox2
+
+
+
+newtype Function a b = Function {
+    function_unbox :: ExpH
+}
+
+instance SmtenT2 Function where
+    smtenT2 x = conT arrowN
+
+instance HaskellF2 Function where
+    box2 = Function
+    unbox2 = function_unbox
+
+applyHF :: (HaskellF a, HaskellF b) => Function a b -> a -> b
+applyHF f x = box ({-# SCC "appHF0" #-} appEH ({-# SCC "appHF_fun" #-} unbox f) ({-# SCC "appHF_arg" #-}unbox x))
+
+lamHF :: (HaskellF a, HaskellF b) => String -> (a -> b) -> Function a b
+lamHF n f =
+  let g :: ExpH -> ExpH
+      g x = {-# SCC "lamHF_G" #-} unbox ({-# SCC "lamHF_G2" #-} ({-# SCC "lamHF_GF" #-} f) ({-# SCC "lamHF_G3" #-} box ({-# SCC "lamHF_G4" #-} x)))
+
+      r = box $ lamEH ({-# SCC "lamHF_Sig" #-} Sig (name n) ta) ({-# SCC "lamHF_tb" #-} tb) g
+      Just (ta, tb) = de_arrowT (smtenT r)
+  in r
+
+smtenHF :: (SmtenEH c, HaskellF f) => c -> f
+smtenHF = box . smtenEH
+
+
+primHF :: (HaskellF a) => Prim -> a
+primHF p = 
+ let z = box $ primEH p (smtenT z)
+ in z
+
+conHF' :: (HaskellF a) => String -> [ExpH] -> a
+conHF' nm args =
+  let r = box $ aconEH (name nm) (smtenT r) args
+  in r
+
+caseHF :: (HaskellF x, HaskellF y, HaskellF n) => String -> x -> y -> n -> n
+caseHF k x y n =
+  let tys = de_arrowsT $ smtenT y
+      tcs = smtenT n
+      tns = de_arrowsT tcs
+      r = box $ caseEH tcs (unbox x) (Sig (name k) t) (unbox y) (unbox n)
+      tx = smtenT x
+      t = arrowsT (take (length tys - length tns) tys ++ [tx])
+  in r
+
 
 newtype List__ a = List____s ExpH
 
