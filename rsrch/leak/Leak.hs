5448d8457740953e0d6d2f0b1cba761a0ac429fb
Richard Uhler <ruhler@csail.mit.edu>
2013-05-24 17:22:23 -0400

    Imported much of HaskellF prelude.

--- rsrch/leak/Leak.hs
+++ rsrch/leak/Leak.hs
@@ -7,21 +7,59 @@
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE KindSignatures #-}
 
-import Prelude hiding (foldr, replicate, (&&), and, Num(..), Eq(..), Bool(..), Integer)
+import Smten.Name
+import Smten.Type
+import Smten.ExpH
+import Smten.Prim
+
+import Prelude hiding (foldr, replicate, (&&), and, Bool(..), Integer) 
 import Smten.HaskellF.HaskellF
-import Smten.HaskellF.Lib.Prelude
+import Smten.HaskellF.Lib.Prelude (
+    List__, __caseCons__, __mkCons__, __mkNil__,
+    )
+newtype Bool = Bool__s ExpH
 
-class HaskellF a => Num a
-    where (-) :: Function a (Function a a)
+instance SmtenT Bool where
+  smtenT _ = conT (name "Prelude.Bool")
 
-instance Num Integer
-    where (-) = __prim_sub_Integer
+instance HaskellF Bool where
+  box = Bool__s
+  unbox x | Bool__s v <- x = v
 
-class HaskellF a => Eq a
-    where (==) :: Function a (Function a Bool)
+__caseTrue :: HaskellF z => Bool -> z -> z -> z
+__caseTrue = caseHF "Prelude.True"
 
-instance Eq Integer
-    where (==) = __prim_eq_Integer
+__caseFalse :: HaskellF z => Bool -> z -> z -> z
+__caseFalse = caseHF "Prelude.False"
+
+__mkTrue :: Bool
+__mkTrue = conHF' "Prelude.True" []
+
+__mkFalse :: Bool
+__mkFalse = conHF' "Prelude.False" []
+
+newtype Integer = Integer__s ExpH
+
+instance SmtenT Integer where
+    smtenT _ = integerT
+
+instance HaskellF Integer where
+    box = Integer__s
+    unbox (Integer__s v) = v
+
+instance Prelude.Num Integer where
+    fromInteger = smtenHF
+    (+) = error $ "+ for haskellf Integer not defined"
+    (*) = error $ "* for haskellf Integer not defined"
+    abs = error $ "abs for haskellf Integer not defined"
+    signum = error $ "signum for haskellf Integer not defined"
+
+__prim_sub_Integer :: Function Integer (Function Integer Integer)
+__prim_sub_Integer = primHF sub_IntegerP
+
+__prim_eq_Integer :: Function Integer (Function Integer Bool)
+__prim_eq_Integer = primHF eq_IntegerP
+
 
 foldr :: (HaskellF a, HaskellF b) => Function (Function a (Function b b)) (Function b (Function (List__ a) b))
 foldr = lamHF "f" $ \f -> lamHF "z" $ \z -> lamHF "l" $ \l ->
@@ -32,8 +70,8 @@ foldr = lamHF "f" $ \f -> lamHF "z" $ \z -> lamHF "l" $ \l ->
 
 replicate ::  HaskellF a => Function Integer (Function a (List__ a))
 replicate = lamHF "n" (\n -> lamHF "x" (\x ->
-  let _s2 = applyHF (applyHF __mkCons__ x) (applyHF (applyHF replicate (applyHF (applyHF (-) n) 1)) x)
-  in __caseTrue (applyHF (applyHF (==) 0) n) __mkNil__ _s2))
+  let _s2 = applyHF (applyHF __mkCons__ x) (applyHF (applyHF replicate (applyHF (applyHF __prim_sub_Integer n) 1)) x)
+  in __caseTrue (applyHF (applyHF __prim_eq_Integer 0) n) __mkNil__ _s2))
 
 and :: Function (List__ Bool) Bool
 and = applyHF (applyHF foldr (&&)) __mkTrue
