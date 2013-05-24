f17d7940eeaf26ebcfd8a718e7b5d46f7a566eba
Richard Uhler <ruhler@csail.mit.edu>
2013-05-24 16:42:16 -0400

    Simplified the code a bit.

--- rsrch/leak/Leak.hs
+++ rsrch/leak/Leak.hs
@@ -7,63 +7,50 @@
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE KindSignatures #-}
 
-import Prelude hiding (foldr, replicate, (&&), and, Num(..), Eq(..), not)
+import Prelude hiding (error, foldr, replicate, (&&), and, Num(..), Eq(..), not, Bool(..), Integer)
 import Smten.HaskellF.HaskellF
 import Smten.HaskellF.Lib.Prelude
 import Smten.HaskellF.Lib.Prelude as Smten.Lib.Prelude
 
-class Smten.HaskellF.HaskellF.HaskellF a => Num a
-    where (+) :: Smten.HaskellF.HaskellF.Function a (Smten.HaskellF.HaskellF.Function a a)
-          (+) = Smten.HaskellF.HaskellF.applyHF (Smten.Lib.Prelude.error :: Smten.HaskellF.HaskellF.Function (Smten.HaskellF.Lib.Prelude.List__ Smten.Lib.Prelude.Char) (Smten.HaskellF.HaskellF.Function a (Smten.HaskellF.HaskellF.Function a a))) (Smten.HaskellF.HaskellF.smtenHF "/home/ruhler/sri/projects/smten/build/home/.cabal/share/smten-2.1.0.0/lib/Prelude.smtn:49:4: no default implementation for (+ :: a -> a -> a)")
-          (*) :: Smten.HaskellF.HaskellF.Function a (Smten.HaskellF.HaskellF.Function a a)
-          (*) = Smten.HaskellF.HaskellF.applyHF (Smten.Lib.Prelude.error :: Smten.HaskellF.HaskellF.Function (Smten.HaskellF.Lib.Prelude.List__ Smten.Lib.Prelude.Char) (Smten.HaskellF.HaskellF.Function a (Smten.HaskellF.HaskellF.Function a a))) (Smten.HaskellF.HaskellF.smtenHF "/home/ruhler/sri/projects/smten/build/home/.cabal/share/smten-2.1.0.0/lib/Prelude.smtn:50:4: no default implementation for (* :: a -> a -> a)")
-          (-) :: Smten.HaskellF.HaskellF.Function a (Smten.HaskellF.HaskellF.Function a a)
-          (-) = Smten.HaskellF.HaskellF.applyHF (Smten.Lib.Prelude.error :: Smten.HaskellF.HaskellF.Function (Smten.HaskellF.Lib.Prelude.List__ Smten.Lib.Prelude.Char) (Smten.HaskellF.HaskellF.Function a (Smten.HaskellF.HaskellF.Function a a))) (Smten.HaskellF.HaskellF.smtenHF "/home/ruhler/sri/projects/smten/build/home/.cabal/share/smten-2.1.0.0/lib/Prelude.smtn:51:9: no default implementation for (- :: a -> a -> a)")
-          negate :: Smten.HaskellF.HaskellF.Function a a
-          negate = Smten.HaskellF.HaskellF.applyHF (Smten.Lib.Prelude.error :: Smten.HaskellF.HaskellF.Function (Smten.HaskellF.Lib.Prelude.List__ Smten.Lib.Prelude.Char) (Smten.HaskellF.HaskellF.Function a a)) (Smten.HaskellF.HaskellF.smtenHF "/home/ruhler/sri/projects/smten/build/home/.cabal/share/smten-2.1.0.0/lib/Prelude.smtn:52:6: no default implementation for (negate :: a -> a)")
-          fromInteger :: Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer a
-          fromInteger = Smten.HaskellF.HaskellF.applyHF (Smten.Lib.Prelude.error :: Smten.HaskellF.HaskellF.Function (Smten.HaskellF.Lib.Prelude.List__ Smten.Lib.Prelude.Char) (Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer a)) (Smten.HaskellF.HaskellF.smtenHF "/home/ruhler/sri/projects/smten/build/home/.cabal/share/smten-2.1.0.0/lib/Prelude.smtn:55:6: no default implementation for (fromInteger :: Integer -> a)")
+class HaskellF a => Num a
+    where (+) :: Function a (Function a a)
+          (-) :: Function a (Function a a)
+          fromInteger :: Function Integer a
 
-instance Num Smten.Lib.Prelude.Integer
-    where (+) :: Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer (Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer Smten.Lib.Prelude.Integer)
-          (+) = Smten.Lib.Prelude.__prim_add_Integer :: Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer (Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer Smten.Lib.Prelude.Integer)
-          (-) :: Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer (Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer Smten.Lib.Prelude.Integer)
-          (-) = Smten.Lib.Prelude.__prim_sub_Integer :: Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer (Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer Smten.Lib.Prelude.Integer)
-          (*) :: Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer (Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer Smten.Lib.Prelude.Integer)
-          (*) = Smten.Lib.Prelude.__prim_mul_Integer :: Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer (Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer Smten.Lib.Prelude.Integer)
-          negate :: Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer Smten.Lib.Prelude.Integer
-          negate = Smten.HaskellF.HaskellF.lamHF "x" (\x -> Smten.HaskellF.HaskellF.applyHF (Smten.HaskellF.HaskellF.applyHF (((-)) :: Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer (Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer Smten.Lib.Prelude.Integer)) (Smten.HaskellF.HaskellF.applyHF (fromInteger :: Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer Smten.Lib.Prelude.Integer) 0)) (x :: Smten.Lib.Prelude.Integer))
-          fromInteger :: Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer Smten.Lib.Prelude.Integer
-          fromInteger = Smten.HaskellF.HaskellF.lamHF "x" (\x -> x :: Smten.Lib.Prelude.Integer)
+instance Num Integer
+    where (+) = __prim_add_Integer
+          (-) = __prim_sub_Integer
+          fromInteger = lamHF "x" (\x -> x :: Integer)
 
-class Smten.HaskellF.HaskellF.HaskellF a => Eq a
-    where (==) :: Smten.HaskellF.HaskellF.Function a (Smten.HaskellF.HaskellF.Function a Smten.HaskellF.Lib.Prelude.Bool)
-          (==) = Smten.HaskellF.HaskellF.lamHF "a" (\a -> Smten.HaskellF.HaskellF.lamHF "b" (\b -> Smten.HaskellF.HaskellF.applyHF (not :: Smten.HaskellF.HaskellF.Function Smten.HaskellF.Lib.Prelude.Bool Smten.HaskellF.Lib.Prelude.Bool) (Smten.HaskellF.HaskellF.applyHF (Smten.HaskellF.HaskellF.applyHF (((/=)) :: Smten.HaskellF.HaskellF.Function a (Smten.HaskellF.HaskellF.Function a Smten.HaskellF.Lib.Prelude.Bool)) (a :: a)) (b :: a))))
-          (/=) :: Smten.HaskellF.HaskellF.Function a (Smten.HaskellF.HaskellF.Function a Smten.HaskellF.Lib.Prelude.Bool)
-          (/=) = Smten.HaskellF.HaskellF.lamHF "a" (\a -> Smten.HaskellF.HaskellF.lamHF "b" (\b -> Smten.HaskellF.HaskellF.applyHF (not :: Smten.HaskellF.HaskellF.Function Smten.HaskellF.Lib.Prelude.Bool Smten.HaskellF.Lib.Prelude.Bool) (Smten.HaskellF.HaskellF.applyHF (Smten.HaskellF.HaskellF.applyHF (((==)) :: Smten.HaskellF.HaskellF.Function a (Smten.HaskellF.HaskellF.Function a Smten.HaskellF.Lib.Prelude.Bool)) (a :: a)) (b :: a))))
+class HaskellF a => Eq a
+    where (==) :: Function a (Function a Bool)
+          (==) = lamHF "a" (\a -> lamHF "b" (\b -> applyHF not (applyHF (applyHF (/=) a) b)))
+          (/=) :: Function a (Function a Bool)
+          (/=) = lamHF "a" (\a -> lamHF "b" (\b -> applyHF not (applyHF (applyHF (==) a) b)))
 
-not :: Smten.HaskellF.HaskellF.Function Smten.HaskellF.Lib.Prelude.Bool Smten.HaskellF.Lib.Prelude.Bool
-not = Smten.HaskellF.HaskellF.lamHF "_p" (\_p -> Smten.Lib.Prelude.__caseTrue (_p :: Smten.HaskellF.Lib.Prelude.Bool) Smten.Lib.Prelude.__mkFalse Smten.Lib.Prelude.__mkTrue)
+not :: Function Bool Bool
+not = lamHF "_p" (\_p -> __caseTrue (_p :: Bool) __mkFalse __mkTrue)
 
-instance Eq Smten.Lib.Prelude.Integer
-    where (==) :: Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer (Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer Smten.HaskellF.Lib.Prelude.Bool)
-          (==) = Smten.Lib.Prelude.__prim_eq_Integer :: Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer (Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer Smten.HaskellF.Lib.Prelude.Bool)
+instance Eq Integer
+    where (==) = Smten.Lib.Prelude.__prim_eq_Integer :: Function Integer (Function Integer Bool)
 
-foldr :: forall a b . (Smten.HaskellF.HaskellF.HaskellF a, Smten.HaskellF.HaskellF.HaskellF b) => Smten.HaskellF.HaskellF.Function (Smten.HaskellF.HaskellF.Function a (Smten.HaskellF.HaskellF.Function b b)) (Smten.HaskellF.HaskellF.Function b (Smten.HaskellF.HaskellF.Function (Smten.HaskellF.Lib.Prelude.List__ a) b))
-foldr = Smten.HaskellF.HaskellF.lamHF "_p" (\_p -> Smten.HaskellF.HaskellF.lamHF "_p1" (\_p1 -> Smten.HaskellF.HaskellF.lamHF "_p2" (\_p2 -> let _s2 :: b = Smten.HaskellF.HaskellF.applyHF (Smten.HaskellF.HaskellF.lamHF "f" (\f -> Smten.HaskellF.HaskellF.applyHF (Smten.HaskellF.HaskellF.lamHF "z" (\z -> z :: b)) (_p1 :: b))) (_p :: Smten.HaskellF.HaskellF.Function a (Smten.HaskellF.HaskellF.Function b b))
-                                                                                                                                              in Smten.HaskellF.HaskellF.applyHF (Smten.HaskellF.HaskellF.lamHF "f" (\f -> Smten.HaskellF.HaskellF.applyHF (Smten.HaskellF.HaskellF.lamHF "z" (\z -> Smten.HaskellF.Lib.Prelude.__caseCons__ (_p2 :: Smten.HaskellF.Lib.Prelude.List__ a) (Smten.HaskellF.HaskellF.lamHF "x" (\x -> Smten.HaskellF.HaskellF.lamHF "xs" (\xs -> Smten.HaskellF.HaskellF.applyHF (Smten.HaskellF.HaskellF.applyHF (f :: Smten.HaskellF.HaskellF.Function a (Smten.HaskellF.HaskellF.Function b b)) (x :: a)) (Smten.HaskellF.HaskellF.applyHF (Smten.HaskellF.HaskellF.applyHF (Smten.HaskellF.HaskellF.applyHF (foldr :: Smten.HaskellF.HaskellF.Function (Smten.HaskellF.HaskellF.Function a (Smten.HaskellF.HaskellF.Function b b)) (Smten.HaskellF.HaskellF.Function b (Smten.HaskellF.HaskellF.Function (Smten.HaskellF.Lib.Prelude.List__ a) b))) (f :: Smten.HaskellF.HaskellF.Function a (Smten.HaskellF.HaskellF.Function b b))) (z :: b)) (xs :: Smten.HaskellF.Lib.Prelude.List__ a))))) (_s2 :: b))) (_p1 :: b))) (_p :: Smten.HaskellF.HaskellF.Function a (Smten.HaskellF.HaskellF.Function b b)))))
-replicate :: forall a . Smten.HaskellF.HaskellF.HaskellF a => Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer (Smten.HaskellF.HaskellF.Function a (Smten.HaskellF.Lib.Prelude.List__ a))
-replicate = Smten.HaskellF.HaskellF.lamHF "_p" (\_p -> Smten.HaskellF.HaskellF.lamHF "_p1" (\_p1 -> let _s2 :: Smten.HaskellF.Lib.Prelude.List__ a = Smten.HaskellF.HaskellF.applyHF (Smten.HaskellF.HaskellF.lamHF "n" (\n -> Smten.HaskellF.HaskellF.applyHF (Smten.HaskellF.HaskellF.lamHF "x" (\x -> Smten.HaskellF.HaskellF.applyHF (Smten.HaskellF.HaskellF.applyHF Smten.HaskellF.Lib.Prelude.__mkCons__ (x :: a)) (Smten.HaskellF.HaskellF.applyHF (Smten.HaskellF.HaskellF.applyHF (replicate :: Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer (Smten.HaskellF.HaskellF.Function a (Smten.HaskellF.Lib.Prelude.List__ a))) (Smten.HaskellF.HaskellF.applyHF (Smten.HaskellF.HaskellF.applyHF (((-)) :: Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer (Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer Smten.Lib.Prelude.Integer)) (n :: Smten.Lib.Prelude.Integer)) (Smten.HaskellF.HaskellF.applyHF (fromInteger :: Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer Smten.Lib.Prelude.Integer) 1))) (x :: a)))) (_p1 :: a))) (_p :: Smten.Lib.Prelude.Integer)
-                                                                                                     in Smten.Lib.Prelude.__caseTrue (Smten.HaskellF.HaskellF.applyHF (Smten.HaskellF.HaskellF.applyHF (((==)) :: Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer (Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer Smten.HaskellF.Lib.Prelude.Bool)) (Smten.HaskellF.HaskellF.applyHF (fromInteger :: Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer Smten.Lib.Prelude.Integer) 0)) (_p :: Smten.Lib.Prelude.Integer)) Smten.HaskellF.Lib.Prelude.__mkNil__ (_s2 :: Smten.HaskellF.Lib.Prelude.List__ a)))
+foldr :: forall a b . (HaskellF a, HaskellF b) => Function (Function a (Function b b)) (Function b (Function (List__ a) b))
+foldr = lamHF "_p" (\_p -> lamHF "_p1" (\_p1 -> lamHF "_p2" (\_p2 -> let _s2 :: b = applyHF (lamHF "f" (\f -> applyHF (lamHF "z" (\z -> z :: b)) (_p1 :: b))) (_p :: Function a (Function b b))
+                                                                     in applyHF (lamHF "f" (\f -> applyHF (lamHF "z" (\z -> __caseCons__ (_p2 :: List__ a) (lamHF "x" (\x -> lamHF "xs" (\xs -> applyHF (applyHF (f :: Function a (Function b b)) (x :: a)) (applyHF (applyHF (applyHF (foldr :: Function (Function a (Function b b)) (Function b (Function (List__ a) b))) (f :: Function a (Function b b))) (z :: b)) (xs :: List__ a))))) (_s2 :: b))) (_p1 :: b))) (_p :: Function a (Function b b)))))
+replicate :: forall a . HaskellF a => Function Integer (Function a (List__ a))
+replicate = lamHF "_p" (\_p -> lamHF "_p1" (\_p1 -> let _s2 :: List__ a = applyHF (lamHF "n" (\n -> applyHF (lamHF "x" (\x -> applyHF (applyHF __mkCons__ (x :: a)) (applyHF (applyHF (replicate :: Function Integer (Function a (List__ a))) (applyHF (applyHF (((-)) :: Function Integer (Function Integer Integer)) (n :: Integer)) (applyHF (fromInteger :: Function Integer Integer) 1))) (x :: a)))) (_p1 :: a))) (_p :: Integer)
+                                                    in __caseTrue (applyHF (applyHF (((==)) :: Function Integer (Function Integer Bool)) (applyHF (fromInteger :: Function Integer Integer) 0)) (_p :: Integer)) __mkNil__ (_s2 :: List__ a)))
 
-and :: Smten.HaskellF.HaskellF.Function (Smten.HaskellF.Lib.Prelude.List__ Smten.HaskellF.Lib.Prelude.Bool) Smten.HaskellF.Lib.Prelude.Bool
-and = Smten.HaskellF.HaskellF.applyHF (Smten.HaskellF.HaskellF.applyHF (foldr :: Smten.HaskellF.HaskellF.Function (Smten.HaskellF.HaskellF.Function Smten.HaskellF.Lib.Prelude.Bool (Smten.HaskellF.HaskellF.Function Smten.HaskellF.Lib.Prelude.Bool Smten.HaskellF.Lib.Prelude.Bool)) (Smten.HaskellF.HaskellF.Function Smten.HaskellF.Lib.Prelude.Bool (Smten.HaskellF.HaskellF.Function (Smten.HaskellF.Lib.Prelude.List__ Smten.HaskellF.Lib.Prelude.Bool) Smten.HaskellF.Lib.Prelude.Bool))) (((&&)) :: Smten.HaskellF.HaskellF.Function Smten.HaskellF.Lib.Prelude.Bool (Smten.HaskellF.HaskellF.Function Smten.HaskellF.Lib.Prelude.Bool Smten.HaskellF.Lib.Prelude.Bool))) Smten.Lib.Prelude.__mkTrue
+and :: Function (List__ Bool) Bool
+and = applyHF (applyHF foldr (&&)) __mkTrue
 
-(&&) :: Smten.HaskellF.HaskellF.Function Smten.HaskellF.Lib.Prelude.Bool (Smten.HaskellF.HaskellF.Function Smten.HaskellF.Lib.Prelude.Bool Smten.HaskellF.Lib.Prelude.Bool)
-(&&) = Smten.HaskellF.HaskellF.lamHF "_p" (\_p -> Smten.HaskellF.HaskellF.lamHF "_p1" (\_p1 -> Smten.Lib.Prelude.__caseTrue (_p :: Smten.HaskellF.Lib.Prelude.Bool) (Smten.HaskellF.HaskellF.applyHF (Smten.HaskellF.HaskellF.lamHF "x" (\x -> x :: Smten.HaskellF.Lib.Prelude.Bool)) (_p1 :: Smten.HaskellF.Lib.Prelude.Bool)) Smten.Lib.Prelude.__mkFalse))
+(&&) :: Function Bool (Function Bool Bool)
+(&&) = lamHF "_p" (\_p -> lamHF "_p1" (\_p1 -> __caseTrue (_p :: Bool) (applyHF (lamHF "x" (\x -> x :: Bool)) (_p1 :: Bool)) __mkFalse))
 
-result :: Smten.HaskellF.Lib.Prelude.Bool
-result = (Smten.HaskellF.HaskellF.applyHF (and :: Smten.HaskellF.HaskellF.Function (Smten.HaskellF.Lib.Prelude.List__ Smten.HaskellF.Lib.Prelude.Bool) Smten.HaskellF.Lib.Prelude.Bool) (Smten.HaskellF.HaskellF.applyHF (Smten.HaskellF.HaskellF.applyHF (replicate :: Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer (Smten.HaskellF.HaskellF.Function Smten.HaskellF.Lib.Prelude.Bool (Smten.HaskellF.Lib.Prelude.List__ Smten.HaskellF.Lib.Prelude.Bool))) (Smten.HaskellF.HaskellF.applyHF (fromInteger :: Smten.HaskellF.HaskellF.Function Smten.Lib.Prelude.Integer Smten.Lib.Prelude.Integer) 400000)) Smten.Lib.Prelude.__mkTrue))
+result :: Bool
+result = applyHF and elems
+
+elems = applyHF (applyHF replicate 400000) __mkTrue
 
 main :: Prelude.IO ()
 main = do
