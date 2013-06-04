76e4af7b1401b15b2114520850ddb3a68a53fa6e
Richard Uhler <ruhler@csail.mit.edu>
2013-06-04 13:21:26 -0400

    Preserve sharing in Assertion.

--- smten/Smten/SMT/Assert.hs
+++ smten/Smten/SMT/Assert.hs
@@ -1,43 +1,87 @@
 
 module Smten.SMT.Assert (Smten.SMT.Assert.assert) where
 
+import Control.Monad.Reader
+import System.Mem.StableName
+
+import qualified Data.HashTable.IO as H
+
 import Smten.SMT.AST as AST
 import qualified Smten.Runtime.Prelude as R
 import Smten.SMT.FreeID
 
+type BoolCache exp = H.BasicHashTable (StableName R.Bool) exp
+type IntegerCache exp = H.BasicHashTable (StableName R.Integer) exp
+
+data AR ctx exp = AR {
+  ar_ctx :: ctx,
+  ar_bools :: BoolCache exp,
+  ar_integers :: IntegerCache exp
+}
+
+type AM ctx exp = ReaderT (AR ctx exp) IO
+
 assert :: (AST ctx exp) => ctx -> R.Bool -> IO ()
 assert ctx p = {-# SCC "Assert" #-} do
-    e <- bool_term ctx p
+    bc <- H.new
+    ic <- H.new
+    e <- runReaderT (def_bool ctx p) (AR ctx bc ic)
     AST.assert ctx e
 
-bool_term :: (AST ctx exp) => ctx -> R.Bool -> IO exp
-bool_term ctx R.True = bool ctx True
-bool_term ctx R.False = bool ctx False
-bool_term ctx (R.BoolVar id) = var ctx (freenm id)
-bool_term ctx (R.BoolMux p a b) = do
-    p' <- bool_term ctx p
-    a' <- bool_term ctx a
-    b' <- bool_term ctx b
-    ite ctx p' a' b'
-bool_term ctx (R.Bool__EqInteger a b) = do
-    a' <- int_term ctx a
-    b' <- int_term ctx b
-    eq_integer ctx a' b'
+use_bool :: (AST ctx exp) => R.Bool -> AM ctx exp exp
+use_bool b = do
+    nm <- liftIO $ makeStableName $! b
+    bc <- asks ar_bools
+    found <- liftIO $ H.lookup bc nm
+    case found of
+        Just v -> return v
+        Nothing -> do
+            ctx <- asks ar_ctx
+            v <- def_bool ctx b
+            liftIO $ H.insert bc nm v
+            return v
 
-int_term :: (AST ctx exp) => ctx -> R.Integer -> IO exp
-int_term ctx (R.Integer i) = integer ctx i
-int_term ctx (R.Integer_Add a b) = do
-    a' <- int_term ctx a
-    b' <- int_term ctx b
-    add_integer ctx a' b'
-int_term ctx (R.Integer_Sub a b) = do
-    a' <- int_term ctx a
-    b' <- int_term ctx b
-    sub_integer ctx a' b'
-int_term ctx (R.IntegerMux__ p a b) = do
-    p' <- bool_term ctx p
-    a' <- int_term ctx a
-    b' <- int_term ctx b
-    ite ctx p' a' b'
-int_term ctx (R.IntegerVar id) = var ctx (freenm id)
+use_int :: (AST ctx exp) => R.Integer -> AM ctx exp exp
+use_int i = do
+    nm <- liftIO $ makeStableName $! i
+    ic <- asks ar_integers
+    found <- liftIO $ H.lookup ic nm
+    case found of
+        Just v -> return v
+        Nothing -> do
+            ctx <- asks ar_ctx
+            v <- def_int ctx i
+            liftIO $ H.insert ic nm v
+            return v
+
+def_bool :: (AST ctx exp) => ctx -> R.Bool -> AM ctx exp exp
+def_bool ctx R.True = liftIO $ bool ctx True
+def_bool ctx R.False = liftIO $ bool ctx False
+def_bool ctx (R.BoolVar id) = liftIO $ var ctx (freenm id)
+def_bool ctx (R.BoolMux p a b) = do
+    p' <- use_bool p
+    a' <- use_bool a
+    b' <- use_bool b
+    liftIO $ ite ctx p' a' b'
+def_bool ctx (R.Bool__EqInteger a b) = do
+    a' <- use_int a
+    b' <- use_int b
+    liftIO $ eq_integer ctx a' b'
+
+def_int :: (AST ctx exp) => ctx -> R.Integer -> AM ctx exp exp
+def_int ctx (R.Integer i) = liftIO $ integer ctx i
+def_int ctx (R.Integer_Add a b) = do
+    a' <- use_int a
+    b' <- use_int b
+    liftIO $ add_integer ctx a' b'
+def_int ctx (R.Integer_Sub a b) = do
+    a' <- use_int a
+    b' <- use_int b
+    liftIO $ sub_integer ctx a' b'
+def_int ctx (R.IntegerMux__ p a b) = do
+    p' <- use_bool p
+    a' <- use_int a
+    b' <- use_int b
+    liftIO $ ite ctx p' a' b'
+def_int ctx (R.IntegerVar id) = liftIO $ var ctx (freenm id)
 
