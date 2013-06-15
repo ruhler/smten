afb9c2fa1cfec4358f115dfd0e63ab70b8477dd8
Richard Uhler <ruhler@csail.mit.edu>
2013-06-15 11:20:03 -0400

    Express sharing very cleanly in output debug.
    Now it's exactly the way I want. Though the implementation is a bit subtle.

--- smten/Smten/Runtime/Debug.hs
+++ smten/Smten/Runtime/Debug.hs
@@ -11,74 +11,104 @@ import Data.IORef
 import Text.PrettyPrint.HughesPJ
 import qualified Smten.AnyKeyMap as A
 
-data DR = DR {
-    dr_seen :: A.AnyKeyMap Integer,
-    dr_nid :: IORef Integer
+data ShareState =
+   SeenOnce             -- ^ We have seen the object once so far.
+ | SeenMulti            -- ^ We have seen the object multiple times,
+                        --   but have not yet shown it.
+ | ShownMulti Integer   -- ^ We have seen the object multiple times,
+                        --   and have already shown it as having the given id.
+
+-- Reader data for determining which expressions are shared.
+data SR = SR {
+    -- Map of expressions seen.
+    sr_seen :: A.AnyKeyMap ShareState
 }
 
-data MyDoc = MyDoc {
-    md_doc :: Doc,
-    md_share :: Bool
+-- Reader data for the doc generation pass.
+data GR = GR {
+    gr_seen :: A.AnyKeyMap ShareState,
+    gr_nid :: IORef Integer
 }
 
-mydoc :: Doc -> MyDoc
-mydoc d = MyDoc d True
+data Debug = Debug {
+    -- The sharing pass.
+    -- In this pass, we determine which expressions are shared.
+    -- The sr_seen map is updated during the traversal.
+    -- The return Bool indicates whether or not this value is shareable.
+    _db_sh :: ReaderT SR IO Bool,
 
-noshare :: Debug -> Debug
-noshare d = do
-  v <- d
-  return (v { md_share = False })
-
-type DebugM = ReaderT DR IO
-type Debug = DebugM MyDoc
+    -- The doc pass.
+    -- Generates the actual document, using the result of sharing analysis
+    -- from the first pass to figure out which expressions to annotate.
+    _db_go :: ReaderT GR IO Doc
+}
 
 dbgShare :: (a -> Debug) -> a -> Debug
-dbgShare f x = do
-  m <- ask
-  fnd <- liftIO $ A.lookup (dr_seen m) x
-  case fnd of
-    Just v -> return $ MyDoc (text "@" <> integer v) False
-    Nothing -> do
-      v <- f x
-      if (md_share v)
-        then do
-          id <- liftIO $ readIORef (dr_nid m)
-          liftIO $ do
-            modifyIORef' (dr_nid m) (+ 1)
-            A.insert (dr_seen m) x id
-          return (MyDoc (text ("@" ++ show id ++ "{") <+> (md_doc v) <+> text "}") False)
-        else do
-          return v
+dbgShare f x =
+  let Debug shx gox = f x
+      sh = do
+          m <- ask
+          fnd <- liftIO $ A.lookup (sr_seen m) x
+          case fnd of
+            Nothing -> do
+                shareable <- shx
+                if shareable
+                    then liftIO $ A.insert (sr_seen m) x SeenOnce >> return False
+                    else return False
+            Just SeenOnce -> liftIO $ A.insert (sr_seen m) x SeenMulti >> return False
+            Just SeenMulti -> return False
+            Just (ShownMulti v) -> error "unexpected ShownMulti in sh pass"
+
+      go = do
+          m <- ask
+          fnd <- liftIO $ A.lookup (gr_seen m) x
+          case fnd of   
+             Just (ShownMulti v) -> return (text "@" <> integer v)
+             Just SeenMulti -> do
+                d <- gox
+                v <- liftIO $ readIORef (gr_nid m)
+                liftIO $ do
+                  modifyIORef' (gr_nid m) (+ 1)
+                  A.insert (gr_seen m) x (ShownMulti v)
+                  return $ text ("@" ++ show v ++ "{") <+> d <+> text "}"
+             _ -> gox
+  in Debug sh go
+
+b :: (Doc -> Doc -> Doc) -> Debug -> Debug -> Debug
+b f (Debug sha goa) (Debug shb gob) =
+  let sh = sha >> shb >> return True 
+      go = do
+        da <- goa
+        db <- gob
+        return $ f da db
+  in Debug sh go
 
 (<+>.) :: Debug -> Debug -> Debug
-(<+>.) a b = do
-    a' <- md_doc <$> a
-    b' <- md_doc <$> b
-    return $ mydoc (a' <+> b')
+(<+>.) = b (<+>)
 
 ($+$.) :: Debug -> Debug -> Debug
-($+$.) a b = do
-    a' <- md_doc <$> a
-    b' <- md_doc <$> b
-    return $ mydoc (a' $+$ b')
+($+$.) = b ($+$)
 
 text' :: String -> Debug 
-text' = return . mydoc . text
+text' s = Debug (return True) (return $ text s)
+
+l :: ([Doc] -> Doc) -> [Debug] -> Debug
+l f xs =
+  let sh = sequence [s | Debug s _ <- xs] >> return True 
+      go = do
+        ds <- sequence [g | Debug _ g <- xs]
+        return (f ds)
+  in Debug sh go
+  
 
 sep' :: [Debug] -> Debug
-sep' ms = do
-  vs <- sequence ms
-  return $ mydoc (sep (map md_doc vs))
+sep' = l sep
 
 vcat' :: [Debug] -> Debug
-vcat' ms = do
-  vs <- sequence ms
-  return $ mydoc (vcat (map md_doc vs))
+vcat' = l vcat
 
 nest' :: Int -> Debug -> Debug
-nest' i x = do 
-  v <- md_doc <$> x
-  return $ mydoc (nest i v)
+nest' i (Debug shx gox) = Debug shx (nest i <$> gox)
 
 dbgOp :: String -> Debug -> Debug -> Debug
 dbgOp op a b = a <+>. text' op <+>. b
@@ -90,7 +120,7 @@ dbgApps :: Debug -> [Debug] -> Debug
 dbgApps x xs = sep' (x:xs)
 
 dbgVar :: String -> Debug
-dbgVar = noshare . text'
+dbgVar = dbgText
 
 tabwidth :: Int
 tabwidth = 2
@@ -108,8 +138,9 @@ dbgCase k x y n
                  text' "_" <+>. text' "->" <+>. n
               ]) $+$. text' "}"
 
+-- Raw text is considered not-shareable.
 dbgText :: String -> Debug
-dbgText = noshare . text'
+dbgText s = Debug (return False) (return $ text s)
 
 dbgError :: String -> Debug
 dbgError s = text' "error" <+>. text' (show s)
@@ -118,12 +149,13 @@ dbgCon :: String -> [Debug] -> Debug
 dbgCon k [] = dbgText k
 dbgCon k xs = sep' ((text' k) : xs)
 
-dbgRender :: Debug -> IO String
-dbgRender d = {-# SCC "DebugRender" #-} do
-  m <- A.new
-  id <- newIORef 0
-  render . md_doc <$> runReaderT d (DR m id)
-
 dbgLit :: (Show a) => a -> Debug
 dbgLit = dbgText . show
+
+dbgRender :: Debug -> IO String
+dbgRender (Debug sh go) = {-# SCC "DebugRender" #-} do
+  m <- A.new
+  v <- newIORef 0
+  _ <- runReaderT sh (SR m)
+  render <$> runReaderT go (GR m v)
 
