855af9a0140abe5cdb7d94e837c1abd2e728ee2b
Richard Uhler <ruhler@csail.mit.edu>
2013-07-30 16:23:17 -0400

    Recognize that a suspected bug was fixed with previous changes.

--- smten-base/Smten/Data/Either.hs
+++ smten-base/Smten/Data/Either.hs
@@ -12,7 +12,7 @@ either :: (a -> c) -> (b -> c) -> Either a b -> c
 either f _ (Left x) = f x
 either _ g (Right y) = g y
 
-instance Functor (Either e) where
+instance Functor (Either a) where
     fmap _ (Left x) = Left x
     fmap f (Right y) = Right (f y)
 
