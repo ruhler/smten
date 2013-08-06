88d0e3a7b988f3fc2adf7726f91fdbdd0d5759b1
Richard Uhler <ruhler@csail.mit.edu>
2013-08-06 14:22:35 -0400

    Added support for -O1 when compiling with the plugin.

--- smten-base/Smten/Compiled/GHC/Base.hs
+++ smten-base/Smten/Compiled/GHC/Base.hs
@@ -1,6 +1,7 @@
 
 {-# LANGUAGE NoImplicitPrelude #-}
-module Smten.Compiled.GHC.Base (eqString) where
+module Smten.Compiled.GHC.Base (eqString, build, foldr) where
 
 import Smten.Compiled.Smten.Data.Char
+import Smten.Compiled.Smten.Data.List0
 
--- smten-base/Smten/Data/List0.hs
+++ smten-base/Smten/Data/List0.hs
@@ -1,7 +1,8 @@
 
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 module Smten.Data.List0 (
-    map, (++), foldr,
+    map, (++), foldr, build,
  ) where
 
 infixr 5 ++
@@ -17,4 +18,7 @@ map f (x:xs) = f x : map f xs
 foldr :: (a -> b -> b) -> b -> [a] -> b
 foldr f z [] = z
 foldr f z (x:xs) = f x (foldr f z xs)
+
+build :: forall a . (forall b. (a -> b -> b) -> b -> b) -> [a]
+build g = g (:) []
 
