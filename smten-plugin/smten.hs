6bb56311600d02caf15b0d0c19053576af107c52
Richard Uhler <ruhler@csail.mit.edu>
2013-10-04 16:46:45 -0400

    Refactor 'smten' tool again.
    Now we are using it for shampi and sketch.

--- smten-plugin/smten.hs
+++ smten-plugin/smten.hs
@@ -6,51 +6,101 @@
 -- This executable is just a wrapper which properly invokes ghc with the
 -- smten-plugin.
 
+import Control.Monad.State
+import Data.List
 import System.Cmd
+import System.Environment
 import System.Exit
-import Smten.Plugin.ParseArgs
+
+usage :: String
+usage = unlines [
+    "Usage:",
+    "  smten --make -o outfile infile.hs [options]",
+    "",
+    "Supported Options:",
+    "  -?, --help           Print this usage information and exit",
+    "  -O                   Enable optimiziation",
+    "  -prof                Enable profiling",
+    "  -fprof-auto-top      Auto-add SCCs to top-level bindings",
+    "  -idir1:dir2:...      Add dir1, dir2, etc. to import path",
+    "  -hidir dir           Set directory for interface files",
+    "  -odir dir            Set directory for object files",
+    ""]
+
+data Mode = Help | Make | Error String
+
+data Options = Options {
+    o_mode :: Mode,
+
+    -- Options for stage 1 call to ghc.
+    o_s1args :: [String],
+
+    -- Options for stage 2 call to ghc.
+    o_s2args :: [String]
+}
+
+type OptionsM = State Options
+
+-- Add a list of stage 1 options.
+s1 :: [String] -> OptionsM ()
+s1 xs = modify $ \s -> s { o_s1args = xs ++ o_s1args s }
+
+-- Add a list of stage 2 options.
+s2 :: [String] -> OptionsM ()
+s2 xs = modify $ \s -> s { o_s2args = xs ++ o_s2args s }
+
+-- Set the mode.
+smode :: Mode -> OptionsM ()
+smode m = modify $ \s -> s { o_mode = m }
+
+getopts :: [String] -> OptionsM ()
+getopts al =
+  case al of
+    [] -> return ()
+    ("--help" : tl) -> smode Help >> getopts tl
+    ("-?" : tl) -> smode Help >> getopts tl
+    ("--make" : tl) -> smode Make >> getopts tl
+    ("-o" : f : tl) -> s2 ["-o", f] >> getopts tl
+    ("-O" : tl) -> s2 ["-O"] >> getopts tl
+    ("-prof" : tl) -> s2 ["-prof"] >> getopts tl
+    ("-fprof-auto-top" : tl) -> s1 ["-fprof-auto-top"] >> getopts tl
+    ("-hidir" : f : tl) -> s1 ["-hidir", f] >> s2 ["-hidir", f] >> getopts tl
+    ("-odir" : f : tl) -> s1 ["-odir", f] >> s2 ["-odir", f] >> getopts tl
+    (x@('-':'i':_) : tl) -> s1 [x] >> s2 [x] >> getopts tl
+    (('-':f) : _) -> smode (Error $ "unrecognized flag: " ++ show ('-':f))
+    (f : tl) -> s1 [f] >> getopts tl
+
+          
 
 main :: IO ()
 main = do
-  args <- parseArgs
+  args <- getArgs
+  let opts = execState (getopts args) (Options Help [] [])
 
-  if sa_help args
-     then putStrLn usage >> exitSuccess 
-     else return ()
+  case o_mode opts of
+    Help -> putStrLn usage >> exitSuccess
+    Error msg -> do
+        putStrLn msg  
+        putStrLn usage
+        exitFailure
+    Make -> do
+      let s1 = ["--make", "-osuf", "smten_o", "-prof",
+                "-fplugin=Smten.Plugin.Plugin",
+                "-XRebindableSyntax", "-XNoImplicitPrelude",
+                "-O0", "-c"] ++ o_s1args opts
+      putStrLn "Stage 1: Compiling using ghc with smten plugin"
+      putStrLn $ "ghc " ++ show s1
+      ec1 <- rawSystem "ghc" s1
+      case ec1 of
+        ExitSuccess -> return ()
+        ExitFailure n -> error $ "State 1 smten compilation failed with: " ++ show n
 
-  let infile = sa_infile args
-      outfile = sa_outfile args
-      profile = sa_profile args
-      optimize = sa_optimize args
-
-  -- Step 1: Compile the code with -fplugin=Smten.Plugin.Plugin
-  let s1args = [infile,
-                 "--make", "-osuf", "smten_o", 
-                 "-hisuf", "smten_hi",
-                 "-prof",
-                 "-fplugin=Smten.Plugin.Plugin",
-                 "-XRebindableSyntax", "-XNoImplicitPrelude",
-                 "-O0", "-c"]
-  putStrLn "Stage 1: Compiling using ghc with smten plugin"
-  putStrLn $ "ghc " ++ show s1args
-  ec1 <- rawSystem "ghc" s1args
-  case ec1 of
-    ExitSuccess -> return ()
-    ExitFailure n -> error $ "State 1 smten compilation failed with: " ++ show n
-
-  -- Step 2: Compile the generated smten code.
-  --   Set -main-is Smten.Compiled.Main.main
-  --   Add -i<odir>
-  let s2args = concat [["-o", outfile,
-                        "Smten/Compiled/Main.hs",
-                        "-main-is", "Smten.Compiled.Main.main"],
-                       if profile then ["-prof"] else [],
-                       if optimize then ["-O"] else []]
-
-  putStrLn "State 2: Compiling generated haskell code without smten plugin"
-  putStrLn $ "ghc " ++ show s2args
-  ec2 <- rawSystem "ghc" s2args
-  case ec2 of
-    ExitSuccess -> return ()
-    ExitFailure n -> error $ "State 2 smten compilation failed with: " ++ show n
+      let s2 = ["--make", "Smten.Compiled.Main",
+                "-main-is", "Smten.Compiled.Main.main"] ++ o_s2args opts
+      putStrLn "State 2: Compiling generated haskell code without smten plugin"
+      putStrLn $ "ghc " ++ show s2
+      ec2 <- rawSystem "ghc" s2
+      case ec2 of
+        ExitSuccess -> return ()
+        ExitFailure n -> error $ "State 2 smten compilation failed with: " ++ show n
 
