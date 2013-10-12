29d40693ec84eb256ea205d7b29dd39fb63ad77d
Richard Uhler <ruhler@csail.mit.edu>
2013-10-12 15:58:56 -0400

    Use bitblaster and int support for the 'pure' solver.

--- smten-lib/Smten/Compiled/Smten/Symbolic/Solver/Pure.hs
+++ smten-lib/Smten/Compiled/Smten/Symbolic/Solver/Pure.hs
@@ -5,34 +5,32 @@ module Smten.Compiled.Smten.Symbolic.Solver.Pure (pure) where
 
 import Control.Monad
 
-import Data.Bits
 import Data.IORef
 import Data.Functor
 
-import Smten.Runtime.Bit
 import Smten.Runtime.Types (Type(..))
 import Smten.Runtime.Result
 import Smten.Runtime.SolverAST
 import Smten.Runtime.Solver
-
-data Any = B Bool
-         | I Integer
-         | BV Bit
-    deriving (Eq)
+import Smten.Runtime.Bits
+import Smten.Runtime.Integers
 
 data Model = Model {
-    vars :: [(String, Any)]
+    vars :: [(String, Bool)]
 }
 
-data Exp = Exp (Model -> Any)
+data Exp = Exp (Model -> Bool)
 
 data PureSolver = PureSolver (IORef [Model])
 
 ite_Exp :: Exp -> Exp -> Exp -> IO Exp
 ite_Exp (Exp p) (Exp a) (Exp b) = return . Exp $ \m ->
      case p m of
-        B True -> a m
-        B False -> b m
+        True -> a m
+        False -> b m
+
+nobits = error "PureSolver: bits not supported natively"
+noints = error "PureSolver: integers not supported natively"
 
 instance SolverAST PureSolver Exp where
   declare (PureSolver mref) BoolT nm = do
@@ -40,14 +38,10 @@ instance SolverAST PureSolver Exp where
      writeIORef mref $ do
         vs <- vars <$> ms
         b <- [True, False]
-        return (Model $ (nm, B b) : vs)
+        return (Model $ (nm, b) : vs)
 
-  -- For now give an error in this case.
-  -- We could just return the list of all integers and hope for the best, but
-  -- for debugging purposes, because I plan not to use that feature, throw an
-  -- error if that is attempted.
-  declare (PureSolver mref) IntegerT nm = error "PureSolver.declare Integer"
-  declare (PureSolver mref) (BitT _) nm = error "PureSolver.declare Bit"
+  declare (PureSolver mref) IntegerT nm = noints
+  declare (PureSolver mref) (BitT _) nm = nobits
 
   getBoolValue (PureSolver mref) nm = do
      ms <- readIORef mref
@@ -55,11 +49,11 @@ instance SolverAST PureSolver Exp where
         [] -> error $ "getBoolValue called when there is no model"
         (x:_) ->
           case lookup nm (vars x) of
-            Just (B b) -> return b
+            Just b -> return b
             Nothing -> error $ "getBoolValue: " ++ nm ++ " not found"
   
-  getIntegerValue = error "PureSolver.getIntegerValue"
-  getBitVectorValue = error "PureSolver.getBitVectorValue"
+  getIntegerValue = noints
+  getBitVectorValue = nobits
 
   check (PureSolver mref) = do
      ms <- readIORef mref
@@ -69,95 +63,49 @@ instance SolverAST PureSolver Exp where
      ms <- readIORef mref
      writeIORef mref $ do
         m <- ms
-        guard (p m == B True)
+        guard (p m == True)
         return m
 
-  bool _ x = return (Exp $ const (B x))
-  integer _ x = return (Exp $ const (I x))
-  bit _ w v = return (Exp $ const (BV (bv_make w v)))
+  bool _ x = return (Exp $ const x)
+  integer = noints
+  bit = nobits
 
   var _ nm = return . Exp $ \m ->
                 case lookup nm (vars m) of
                     Just x -> x
                     Nothing -> error $ "var: " ++ nm ++ " not found"
 
-  and_bool ctx a b = ite_bool ctx a b (Exp $ const (B False))
-  not_bool ctx a = ite_bool ctx a (Exp $ const (B False)) (Exp $ const (B True))
+  and_bool ctx a b = ite_bool ctx a b (Exp $ const False)
+  not_bool ctx a = ite_bool ctx a (Exp $ const False) (Exp $ const True)
 
   ite_bool _ = ite_Exp
-  ite_integer _ = ite_Exp
-  ite_bit _ = ite_Exp
+  ite_integer = noints
+  ite_bit _ = nobits
 
-  eq_integer _ (Exp a) (Exp b) = return . Exp $ \m ->
-     case (a m, b m) of
-        (I av, I bv) -> B (av == bv)
+  eq_integer = noints
+  leq_integer = noints
+  add_integer = noints
+  sub_integer = noints
 
-  leq_integer _ (Exp a) (Exp b) = return . Exp $ \m ->
-     case (a m, b m) of
-        (I av, I bv) -> B (av <= bv)
-
-  add_integer _ (Exp a) (Exp b) = return . Exp $ \m ->
-     case (a m, b m) of
-        (I av, I bv) -> I (av + bv)
-
-  sub_integer _ (Exp a) (Exp b) = return . Exp $ \m ->
-     case (a m, b m) of
-        (I av, I bv) -> I (av - bv)
-
-  eq_bit _ (Exp a) (Exp b) = return . Exp $ \m ->
-     case (a m, b m) of
-        (BV av, BV bv) -> B (av == bv)
-
-  leq_bit _ (Exp a) (Exp b) = return . Exp $ \m ->
-     case (a m, b m) of
-        (BV av, BV bv) -> B (av <= bv)
-
-  add_bit _ (Exp a) (Exp b) = return . Exp $ \m ->
-     case (a m, b m) of
-        (BV av, BV bv) -> BV (av + bv)
-
-  sub_bit _ (Exp a) (Exp b) = return . Exp $ \m ->
-     case (a m, b m) of
-        (BV av, BV bv) -> BV (av - bv)
-
-  mul_bit _ (Exp a) (Exp b) = return . Exp $ \m ->
-     case (a m, b m) of
-        (BV av, BV bv) -> BV (av * bv)
-
-  or_bit _ (Exp a) (Exp b) = return . Exp $ \m ->
-     case (a m, b m) of
-        (BV av, BV bv) -> BV (av .|. bv)
-
-  and_bit _ (Exp a) (Exp b) = return . Exp $ \m ->
-     case (a m, b m) of
-        (BV av, BV bv) -> BV (av .&. bv)
-
-  shl_bit _ _ (Exp a) (Exp b) = return . Exp $ \m ->
-     case (a m, b m) of
-        (BV av, BV bv) -> BV (av `bv_shl` bv)
-
-  lshr_bit _ _ (Exp a) (Exp b) = return . Exp $ \m ->
-     case (a m, b m) of
-        (BV av, BV bv) -> BV (av `bv_lshr` bv)
-
-  concat_bit _ (Exp a) (Exp b) = return . Exp $ \m ->
-     case (a m, b m) of
-        (BV av, BV bv) -> BV (av `bv_concat` bv)
-
-  not_bit _ (Exp a) = return . Exp $ \m ->
-     case a m of
-        BV av -> BV (complement av)
-
-  sign_extend_bit _ fr to (Exp b) = return . Exp $ \m ->
-    case b m of
-        BV bv -> BV (bv_sign_extend (to - fr) bv)
-
-  extract_bit _ hiv lov (Exp x) = return . Exp $ \m ->
-    case x m of
-        BV xv -> BV (bv_extract hiv lov xv)
+  eq_bit = nobits
+  leq_bit = nobits
+  add_bit = nobits
+  sub_bit = nobits
+  mul_bit = nobits
+  or_bit = nobits
+  and_bit = nobits
+  shl_bit = nobits
+  lshr_bit = nobits
+  concat_bit = nobits
+  not_bit = nobits
+  sign_extend_bit = nobits
+  extract_bit = nobits
 
 pure :: Solver
 pure = do
    mref <- newIORef [Model []]
-   return $ solverInstFromAST (PureSolver mref)
+   let base = PureSolver mref
+   withints <- addIntegers base
+   withbits <- addBits withints
+   return $ solverInstFromAST withbits
 
