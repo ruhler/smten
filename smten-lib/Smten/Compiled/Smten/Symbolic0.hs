c308cb7af5d4497e9635241d4cd444162e569b51
Richard Uhler <ruhler@csail.mit.edu>
2013-10-15 09:27:27 -0400

    Reimplement the Symbolic monad tracking mzero explicitly.
    This avoids introducing spurious error variables for the result of an mzero
    and avoids introducing boolean variables to choose between mzero and some
    other symbolic computation.
    
    Performance seems much improved in cases involving mzero. For example, sudoku
    with enum and minisat improved by 3x. shampi performance appears unchanged.

--- smten-lib/Smten/Compiled/Smten/Symbolic0.hs
+++ smten-lib/Smten/Compiled/Smten/Symbolic0.hs
@@ -9,9 +9,8 @@ module Smten.Compiled.Smten.Symbolic0 (
     free_Integer, free_Bit,
     ) where
 
-import Control.Monad.Reader
-import Control.Monad.State
-import Data.Functor((<$>))
+import Data.Functor
+import Data.Monoid (mappend, mempty)
 
 import Smten.Runtime.Bit
 import Smten.Runtime.FreeID
@@ -26,24 +25,67 @@ import Smten.Compiled.GHC.TypeLits
 import qualified Smten.Compiled.Smten.Smten.Integer as S
 import qualified Smten.Runtime.Types as S
 
-data SR = SR {
-    sr_pred :: S.Bool
+data Sym a =
+   MZero
+ | Return {
+     -- The result of this computation:
+     _value :: a,
+
+     -- The condition required for this computation:
+     _pred :: S.Bool,
+
+     -- The free variables declared in this computation:
+     _free :: [(FreeID, Type)]
+}
+     
+newtype Symbolic a = Symbolic {
+    -- Run a symbolic computation.
+    -- Note: This is in the IO monad in order to allocate globally fresh
+    -- variable names. We could probably switch to State or ST instead.
+    runS :: IO (Sym a)
 }
 
-data SS = SS {
-    ss_free :: [(FreeID, Type)],
-    ss_formula :: S.Bool
-}
+instance Functor Symbolic where
+    fmap f x = Symbolic $ do
+       sx <- runS x
+       case sx of
+         MZero -> return MZero
+         Return v p fr -> return $ Return (f v) p fr
 
-type Symbolic = ReaderT SR (StateT SS IO)
+instance Monad Symbolic where
+    return x = Symbolic . return $ Return x S.True mempty
+
+    (>>=) x f = Symbolic $ do
+        sx <- runS x
+        case sx of
+            MZero -> return MZero
+            Return v p fr -> do
+                sfv <- runS (f v)
+                case sfv of
+                    MZero -> return MZero
+                    Return fv fp ffr -> do
+                      return $ Return fv (p `andB` fp) (fr `mappend` ffr)
 
 instance SmtenHS1 Symbolic where
+    -- TODO: this should indicate that it may fail.
+    -- That is, running (error) should not return Just error, it should 
+    -- return error.
     error1 msg = return (error0 msg)
 
-    ite1 p a b = do
-      va <- predicated p a
-      vb <- predicated (notB p) b
-      return (ite p va vb)
+    ite1 S.True a _ = a
+    ite1 S.False _ b = b
+    ite1 p a b = Symbolic $ do
+      sa <- runS a
+      sb <- runS b
+      case (sa, sb) of
+        (MZero, MZero) -> return MZero
+        (MZero, Return vb pb fb) -> return $ Return vb (notB p `andB` pb) fb 
+        (Return va pa fa, MZero) -> return $ Return va (p `andB` pa) fa
+        (Return va pa fa, Return vb pb fb) -> do
+          let v = ite0 p va vb
+              p = iteB p pa pb
+              f = fa `mappend` fb
+          return $ Return v p f
 
     realize1 m x = realize m <$> x
 
@@ -54,54 +96,56 @@ bind_symbolic :: Symbolic a -> (a -> Symbolic b) -> Symbolic b
 bind_symbolic = (>>=)
 
 mzero_symbolic :: (SmtenHS0 a) => Symbolic a
-mzero_symbolic = do
-    p <- asks sr_pred
-    modify $ \ss -> ss { ss_formula = ss_formula ss `andB` notB p }
-    return (error0 (errstr "mzero_symbolic"))
+mzero_symbolic = Symbolic $ return MZero
 
 mplus_symbolic :: (SmtenHS0 a) => Symbolic a -> Symbolic a -> Symbolic a
-mplus_symbolic a b = do
-    fid <- liftIO fresh
-    modify $ \s -> s { ss_free = (fid, BoolT) : ss_free s }
-    ite0 (S.Bool_Var fid) a b
+mplus_symbolic a b = Symbolic $ do
+    sa <- runS a
+    sb <- runS b
+    case (sa, sb) of
+       (MZero, _) -> return sb
+       (_, MZero) -> return sa
+       (Return va pa fa, Return vb pb fb) -> do
+         fid <- fresh
+         let v = ite0 (S.Bool_Var fid) va vb
+             p = pa `andB` pb
+             f = (fid, BoolT) : (fa `mappend` fb)
+         return $ Return v p f
 
 free_Integer :: Symbolic (S.Integer)
-free_Integer = do
-    fid <- liftIO fresh
-    modify $ \s -> s { ss_free = (fid, IntegerT) : ss_free s }
-    return (S.Integer_Var fid)
+free_Integer = Symbolic $ do
+    fid <- fresh
+    return $ Return (S.Integer_Var fid) S.True [(fid, IntegerT)]
 
 free_Bit :: SingI Nat n -> Symbolic (S.Bit n)
-free_Bit w = do
-    fid <- liftIO fresh
-    modify $ \s -> s { ss_free = (fid, BitT (__deNewTyDGSingI w)) : ss_free s }
-    return (S.Bit_Var fid)
-
-predicated :: S.Bool -> Symbolic a -> Symbolic a
-predicated p = local (\r -> r { sr_pred = sr_pred r `andB` p })
+free_Bit w = Symbolic $ do
+    fid <- fresh
+    return $ Return (S.Bit_Var fid) S.True [(fid, BitT (__deNewTyDGSingI w))]
 
 run_symbolic :: (SmtenHS0 a) => Solver -> Symbolic a -> IO (S.Maybe a)
 run_symbolic s q = do
-  solver <- s
-  (x, ss) <- runStateT (runReaderT q (SR S.True)) (SS [] S.True)
-  mapM_ (declVar solver) (ss_free ss)
-  assert solver (ss_formula ss)
-  res <- check solver
-  case res of
-    Sat -> do
-       let vars = ss_free ss
-       vals <- mapM (getValue solver) vars
-       m <- model $ zip (map fst vars) vals
-       case {-# SCC "DoubleCheck" #-} realize m (ss_formula ss) of
-          S.True -> return ()
-          S.Bool_Err msg -> doerr msg
-          x -> error $ "SMTEN INTERNAL ERROR: SMT solver lied?"
-                 ++ " Got: " ++ show x
-       cleanup solver
-       return (S.Just ({-# SCC "Realize" #-} realize m x))
-    Unsat -> do
-       cleanup solver
-       return S.Nothing
+  sq <- runS q
+  case sq of
+    MZero -> return S.Nothing
+    Return x p f -> do
+      solver <- s
+      mapM_ (declVar solver) f
+      assert solver p
+      res <- check solver
+      case res of
+        Sat -> do
+           vals <- mapM (getValue solver) f
+           m <- model $ zip (map fst f) vals
+           case {-# SCC "DoubleCheck" #-} realize m p of
+              S.True -> return ()
+              S.Bool_Err msg -> doerr msg
+              x -> error $ "SMTEN INTERNAL ERROR: SMT solver lied?"
+                     ++ " Got: " ++ show x
+           cleanup solver
+           return (S.Just ({-# SCC "Realize" #-} realize m x))
+        Unsat -> do
+           cleanup solver
+           return S.Nothing
 
 getValue :: SolverInst -> (FreeID, Type) -> IO Any
 getValue s (f, BoolT) = do
