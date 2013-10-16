27e422171db0a134e3c9e626a51a9ff1b5abf6b9
Richard Uhler <ruhler@csail.mit.edu>
2013-10-16 11:27:53 -0400

    Remove unused yices1 FFI bindings.
    In preparation for loading yices1 dynamically at runtime.

--- smten-yices1/Smten/Runtime/Yices1FFI.hs
+++ smten-yices1/Smten/Runtime/Yices1FFI.hs
@@ -39,29 +39,14 @@ foreign import ccall "yices1_del_context"
 foreign import ccall "yices1_parse_command"
     c_yices_parse_command :: Ptr YContext -> CString -> IO Bool
 
-foreign import ccall "yices1_parse_expression"
-    c_yices_parse_expression :: Ptr YContext -> CString -> IO YExpr
-
 foreign import ccall "yices1_check"
     c_yices_check :: Ptr YContext -> IO YBool
-
-foreign import ccall "yices1_push"
-    c_yices_push :: Ptr YContext -> IO ()
-
-foreign import ccall "yices1_pop"
-    c_yices_pop :: Ptr YContext -> IO ()
 
 foreign import ccall "yices1_assert"
     c_yices_assert :: Ptr YContext -> YExpr -> IO ()
 
 foreign import ccall "yices1_get_model"
     c_yices_get_model :: Ptr YContext -> IO YModel
-
-foreign import ccall "yices1_display_model"
-    c_yices_display_model :: YModel -> IO ()
-
-foreign import ccall "yices1_enable_type_checker"
-    c_yices_enable_type_checker :: Bool -> IO ()
 
 foreign import ccall "yices1_get_last_error_message"
     c_yices_get_last_error_message :: IO CString
@@ -81,12 +66,6 @@ foreign import ccall "yices1_get_var_decl_from_name"
 foreign import ccall "yices1_mk_var_from_decl"
     c_yices_mk_var_from_decl :: Ptr YContext -> YDecl -> IO YExpr
 
-foreign import ccall "yices1_mk_function_update"
-    c_yices_mk_function_update :: Ptr YContext -> YExpr -> Ptr YExpr -> CUInt -> YExpr -> IO YExpr
-
-foreign import ccall "yices1_mk_app"
-    c_yices_mk_app :: Ptr YContext -> YExpr -> Ptr YExpr -> CUInt -> IO YExpr
-
 foreign import ccall "yices1_mk_true"
     c_yices_mk_true :: Ptr YContext -> IO YExpr
 
@@ -99,9 +78,6 @@ foreign import ccall "yices1_mk_not"
 foreign import ccall "yices1_mk_bv_not"
     c_yices_mk_bv_not :: Ptr YContext -> YExpr -> IO YExpr
 
-foreign import ccall "yices1_mk_or"
-    c_yices_mk_or :: Ptr YContext -> Ptr YExpr -> CUInt -> IO YExpr
-
 foreign import ccall "yices1_mk_and"
     c_yices_mk_and :: Ptr YContext -> Ptr YExpr -> CUInt -> IO YExpr
 
@@ -111,26 +87,14 @@ foreign import ccall "yices1_mk_sum"
 foreign import ccall "yices1_mk_sub"
     c_yices_mk_sub :: Ptr YContext -> Ptr YExpr -> CUInt -> IO YExpr
 
-foreign import ccall "yices1_mk_mul"
-    c_yices_mk_mul :: Ptr YContext -> Ptr YExpr -> CUInt -> IO YExpr
-
 foreign import ccall "yices1_mk_ite"
     c_yices_mk_ite :: Ptr YContext -> YExpr -> YExpr -> YExpr -> IO YExpr
 
 foreign import ccall "yices1_mk_eq"
     c_yices_mk_eq :: Ptr YContext -> YExpr -> YExpr -> IO YExpr
 
-foreign import ccall "yices1_mk_lt"
-    c_yices_mk_lt :: Ptr YContext -> YExpr -> YExpr -> IO YExpr
-
 foreign import ccall "yices1_mk_le"
     c_yices_mk_le :: Ptr YContext -> YExpr -> YExpr -> IO YExpr
-
-foreign import ccall "yices1_mk_gt"
-    c_yices_mk_gt :: Ptr YContext -> YExpr -> YExpr -> IO YExpr
-
-foreign import ccall "yices1_mk_ge"
-    c_yices_mk_ge :: Ptr YContext -> YExpr -> YExpr -> IO YExpr
 
 foreign import ccall "yices1_mk_bv_add"
     c_yices_mk_bv_add :: Ptr YContext -> YExpr -> YExpr -> IO YExpr
@@ -156,17 +120,8 @@ foreign import ccall "yices1_mk_bv_sign_extend"
 foreign import ccall "yices1_mk_bv_extract"
     c_yices_mk_bv_extract :: Ptr YContext -> CUInt -> CUInt -> YExpr -> IO YExpr
 
-foreign import ccall "yices1_mk_bv_lt"
-    c_yices_mk_bv_lt :: Ptr YContext -> YExpr -> YExpr -> IO YExpr
-
-foreign import ccall "yices1_mk_bv_gt"
-    c_yices_mk_bv_gt :: Ptr YContext -> YExpr -> YExpr -> IO YExpr
-
 foreign import ccall "yices1_mk_bv_le"
     c_yices_mk_bv_le :: Ptr YContext -> YExpr -> YExpr -> IO YExpr
-
-foreign import ccall "yices1_mk_bv_ge"
-    c_yices_mk_bv_ge :: Ptr YContext -> YExpr -> YExpr -> IO YExpr
 
 foreign import ccall "yices1_mk_num"
     c_yices_mk_num :: Ptr YContext -> CInt -> IO YExpr
