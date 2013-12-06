596f31550121a96edc39eb2dabb221c6b3061bc9
Richard Uhler <ruhler@csail.mit.edu>
2013-12-06 15:37:05 -0500

    Tested fairness for Int and Char.
    They work fine.

--- smten-lib/Smten/Tests/SMT/Error.hs
+++ smten-lib/Smten/Tests/SMT/Error.hs
@@ -29,18 +29,18 @@ smttests = do
       p <- free_Bool
       assert (if p then error "SMT.Error.Fair2 error hit" else True)
 
-   symtesteq "SMT.Error.INoFair" (Just ()) $ do
+   symtesteq "SMT.Error.IntegerNoFair" (Just ()) $ do
       p <- free_Bool
       assert p
-      assert ((if p then 1 else error "SMT.Error.INoFair error hit") == (1 :: Integer))
+      assert ((if p then 1 else error "SMT.Error.IntegerNoFair error hit") == (1 :: Integer))
 
-   symtesteq "SMT.Error.IFair1" (Just ()) $ do
+   symtesteq "SMT.Error.IntegerFair1" (Just ()) $ do
       p <- free_Bool
-      assert ((if p then 1 else error "SMT.Error.IFair1 error hit") == (1 :: Integer))
+      assert ((if p then 1 else error "SMT.Error.IntegerFair1 error hit") == (1 :: Integer))
 
-   symtesteq "SMT.Error.IFair2" (Just ()) $ do
+   symtesteq "SMT.Error.IntegerFair2" (Just ()) $ do
       p <- free_Bool
-      assert ((if p then error "SMT.Error.IFair1 error hit" else 1) == (1 :: Integer))
+      assert ((if p then error "SMT.Error.IntegerFair1 error hit" else 1) == (1 :: Integer))
 
    symtesteq "SMT.Error.BitNoFair" (Just ()) $ do
       p <- free_Bool
@@ -73,3 +73,30 @@ smttests = do
       if p
         then error "SMT.Error.SymFair2 error hit"
         else return ()
+
+   symtesteq "SMT.Error.IntNoFair" (Just ()) $ do
+      p <- free_Bool
+      assert p
+      assert ((if p then 1 else error "SMT.Error.IntNoFair error hit") == (1 :: Int))
+
+   symtesteq "SMT.Error.IntFair1" (Just ()) $ do
+      p <- free_Bool
+      assert ((if p then 1 else error "SMT.Error.IntFair1 error hit") == (1 :: Int))
+
+   symtesteq "SMT.Error.IntFair2" (Just ()) $ do
+      p <- free_Bool
+      assert ((if p then error "SMT.Error.IntFair1 error hit" else 1) == (1 :: Int))
+
+   symtesteq "SMT.Error.CharNoFair" (Just ()) $ do
+      p <- free_Bool
+      assert p
+      assert ((if p then 'a' else error "SMT.Error.CharNoFair error hit") == 'a')
+
+   symtesteq "SMT.Error.CharFair1" (Just ()) $ do
+      p <- free_Bool
+      assert ((if p then 'a' else error "SMT.Error.CharFair1 error hit") == 'a')
+
+   symtesteq "SMT.Error.CharFair2" (Just ()) $ do
+      p <- free_Bool
+      assert ((if p then error "SMT.Error.CharFair1 error hit" else 'a') == 'a')
+
